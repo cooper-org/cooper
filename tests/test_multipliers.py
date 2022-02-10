0195675bf933ac7904be2cc81b45a840f1afaee1
Jose Gallego-Posada <jgalle29@gmail.com>
2022-02-10 16:24:00 -0500

    Add test for multiplier projection

--- tests/test_multipliers.py
+++ tests/test_multipliers.py
@@ -10,13 +10,31 @@ def test_multipliers_init():
 
     init_tensor = torch.randn(100, 1)
     multiplier = torch_coop.DenseMultiplier(init_tensor)
+    multiplier.project_()
     assert torch.allclose(multiplier(), init_tensor)
 
     init_tensor = torch.tensor([0.0, -1.0, 2.5])
     multiplier = torch_coop.DenseMultiplier(init_tensor)
+    multiplier.project_()
     assert torch.allclose(multiplier(), init_tensor)
 
     # For inequality constraints, the multipliers should be non-negative
     init_tensor = torch.tensor([0.1, -1.0, 2.5])
     multiplier = torch_coop.DenseMultiplier(init_tensor, positive=True)
+    multiplier.project_()
     assert torch.allclose(multiplier(), torch.tensor([0.1, 0.0, 2.5]))
+
+
+def test_custom_projection():
+    class CustomProjectionMultiplier(torch_coop.DenseMultiplier):
+        def project_(self):
+            # Project multipliers so that maximum non-zero entry is exactly 1
+            max_entry = torch.relu(self.weight).max()
+            if max_entry > 0:
+                self.weight.data = self.weight.data / max_entry
+
+    init_tensor = torch.randn(100, 1)
+    multiplier = CustomProjectionMultiplier(init_tensor)
+
+    multiplier.project_()
+    assert torch.allclose(multiplier.weight.data.max(), torch.tensor([1.0]))
