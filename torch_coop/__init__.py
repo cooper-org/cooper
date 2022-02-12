99ee264d7f2bc8857bcd025091f820209d33b1e6
Jose Gallego-Posada <jgalle29@gmail.com>
2022-02-12 04:26:42 -0500

    Edit package init and test config

--- torch_coop/__init__.py
+++ torch_coop/__init__.py
@@ -6,3 +6,10 @@ __version__ = "0.1.0"
 
 from .torch_coop import *
 from . import optim
+
+# from .optimizer import *
+# from .problem import *
+
+from torch_coop.optimizer import ConstrainedOptimizer
+from torch_coop.problem import CMPState, ConstrainedMinimizationProblem
+from torch_coop.lagrangian_formulation import _LagrangianFormulation
