b09c7e8b2293e79604023c38341e381bd39f3c1a
Jose Gallego-Posada <jgalle29@gmail.com>
2022-02-13 23:32:01 -0500

    Remove old version (non-OOP) of ConstrainedOptimizer

--- torch_coop/__init__.py
+++ torch_coop/__init__.py
@@ -4,12 +4,8 @@ __author__ = """Jose Gallego-Posada"""
 __email__ = "jgalle29@gmail.com"
 __version__ = "0.1.0"
 
-from .torch_coop import *
-from . import optim
-
-# from .optimizer import *
-# from .problem import *
-
 from torch_coop.optimizer import ConstrainedOptimizer
 from torch_coop.problem import CMPState, ConstrainedMinimizationProblem
 from torch_coop.lagrangian_formulation import LagrangianFormulation
+
+from . import optim
