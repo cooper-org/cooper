6ca5c188ded831d1a0e863ae4c046d7e206ed107
Jose Gallego-Posada <jgalle29@gmail.com>
2022-02-13 23:50:26 -0500

    Add test for proxy constraints

--- torch_coop/optimizer.py
+++ torch_coop/optimizer.py
@@ -103,6 +103,8 @@ class ConstrainedOptimizer(torch.optim.Optimizer):
         # A currently *positive* gradient means original defect is negative, so
         # the constraint is being satisfied.
 
+        # The code below still works in the case of proxy constraints, since the
+        # multiplier updates are computed based on *non-proxy* constraints
         feasible_filter = self.formulation.ineq_multipliers.weight.grad > 0
         self.formulation.ineq_multipliers.weight.grad[feasible_filter] = 0.0
         self.formulation.ineq_multipliers.weight.data[feasible_filter] = 0.0
