7ffa4bf91f67cce116a1e55929072848db32687f
Jose Gallego <jgalle29@gmail.com>
2022-02-19 22:04:48 -0500

    Merge extrapolation and optimizer aliases

--- torch_coop/optim.py
+++ torch_coop/optim.py
@@ -1,8 +1,49 @@
 """(Extrapolation) Optimizer aliases"""
 
-from .extra_optimizer import extrapolation_class
 import torch
 
+
+def extrapolation_class(optimizer_class):
+    class ExtraOptimizer(optimizer_class):
+        def __init__(self, *args, **kwargs):
+            self.old_iterate = []
+            super().__init__(*args, **kwargs)
+
+        @torch.no_grad()
+        def extrapolation(self, _):
+            """Performs the extrapolation step and save a copy of the current
+            parameters for the update step."""
+            if self.old_iterate:
+                raise RuntimeError(
+                    "Need to call step before calling extrapolation again."
+                )
+            for group in self.param_groups:
+                for p in group["params"]:
+                    self.old_iterate.append(p.detach().clone())
+
+            # Move to extrapolation point
+            super().step()
+
+        @torch.no_grad()
+        def step(self, closure=None):
+            if len(self.old_iterate) == 0:
+                raise RuntimeError("Must call extrapolation before step.")
+
+            i = -1
+            for group in self.param_groups:
+                for p in group["params"]:
+                    i += 1
+                    # Move back to the previous point
+                    p.data = self.old_iterate[i].data
+            super().step()
+
+            # Free the old parameters
+            self.old_iterate.clear()
+
+    return ExtraOptimizer
+
+
+# Define aliases
 SGD = torch.optim.SGD
 Adam = torch.optim.Adam
 Adagrad = torch.optim.Adagrad
