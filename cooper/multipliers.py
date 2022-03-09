aa1678abc0a740011652eff5329d845cc3bd2242
juan43ramirez <juan43.ramirez@gmail.com>
2022-03-09 15:48:01 -0500

    multipliers docs

--- cooper/multipliers.py
+++ cooper/multipliers.py
@@ -1,12 +1,46 @@
 #!/usr/bin/env python
 
-"""Class for Lagrange multipliers."""
+"""Classes for modeling dual variables (e.g. Lagrange multipliers)."""
 
 import torch
 
 
+class BaseMultiplier(torch.nn.Module):
+    """
+    Placeholder for base multiplier class, which can be extended to adapt
+    different types of multipliers: Dense, Sparse, implicit multiplier which is
+    itself the output of a model.
+
+    .. todo::
+        Implement BaseMultiplier class and refactor DenseMultiplier.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+
 class DenseMultiplier(torch.nn.Module):
-    def __init__(self, init, *, positive=False):
+    """
+    A dense multiplier. Holds a :py:class:`~torch.nn.parameter.Parameter`,
+    which contains the value of the Lagrange multipliers associated with the
+    equality or inequality constraints of a
+    :py:class:`~cooper.problem.ConstrainedMinimizationProblem`.
+
+    Args:
+        init: Initial value of the multiplier.
+        positive: Whether to enforce non-negativity on the values of the
+            multiplier.
+
+    Attributes:
+        shape: shape of the :py:class:`~torch.nn.parameter.Parameter` associated
+            with the multiplier.
+        data: data (weight) of the :py:class:`~torch.nn.parameter.Parameter`
+            associated with the multiplier.
+        grad: gradient of the :py:class:`~torch.nn.parameter.Parameter`
+            associated with the multiplier.
+    """
+
+    def __init__(self, init: torch.Tensor, *, positive: bool = False):
         super().__init__()
         self.weight = torch.nn.Parameter(init)
         self.positive = positive
@@ -28,11 +62,17 @@ class DenseMultiplier(torch.nn.Module):
         self.weight.data = value
 
     def forward(self):
+        """
+        Defines the computation performed at every call, which gets the current
+        value of the multiplier.
+        """
         return self.weight
 
     def project_(self):
-        # Generic projection for non-negative multipliers used in inequality
-        # constraints. May be generalized to other custom projections
+        """
+        Generic projection for non-negative multipliers used in inequality
+        constraints. May be generalized to other custom projections.
+        """
         if self.positive:
             self.weight.data = torch.relu(self.data)
 
--- docs/source/multipliers.rst
+++ docs/source/multipliers.rst
@@ -1,8 +1,30 @@
 Multipliers
 ===========
 
-.. currentmodule:: cooper.multipliers
+.. automodule:: cooper.multipliers
+
+Constructing a DenseMultiplier
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+To construct a ``DenseMultiplier``, you have to provide it with an initial
+value. The shape of this initial value must match the shape of the constraint
+defect it corresponds to.
+
+The positivity argument should be specified when initializing the multiplier.
+If set to ``True``, it enforces multipliers to be positive. For multipliers
+associated with inequality constraints, you must specify ``positive=True``.
+
+.. note::
+
+    Multipliers are designed to be mostly handled by
+    :py:class:`~cooper.problem.Formulation`\s. This includes:
+
+    - Their initialization in  in :py:class:`~cooper.problem.BaseLagrangianFormulation`'s :py:meth:`~cooper.problem.BaseLagrangianFormulation.create_state`.
+    - Ensuring that their shape and device matches that of constraint defects given by the :py:class:`~cooper.problem.CMPState` of the ``ConstrainedMinimizationProblem`` they are solving.
+    - Using them for computing Lagrangians in :py:class:`~cooper.problem.LagrangianFormulation`'s :py:meth:`~cooper.problem.LagrangianFormulation.composite_objective`.
 
 .. autoclass:: DenseMultiplier
     :members:
-    :special-members: __init__
+
+.. autoclass:: BaseMultiplier
+    :members:
