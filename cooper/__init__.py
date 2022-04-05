c3bea19a5000d28f7f8f7520a2bbdfe1fffacaca
juan43ramirez <juan43.ramirez@gmail.com>
2022-04-05 17:08:18 -0500

    python3.7

--- cooper/__init__.py
+++ cooper/__init__.py
@@ -1,6 +1,11 @@
 """Top-level package for Constrained Optimization in Pytorch."""
 
-from importlib.metadata import PackageNotFoundError, version
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib.metadata import PackageNotFoundError, version
+else:
+    from importlib_metadata import PackageNotFoundError, version
 
 try:
     __version__ = version("cooper")
