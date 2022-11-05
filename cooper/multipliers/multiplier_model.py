bba4cb1b692712a8e91459238fc9fb373d04fcb2
IsitaRex <41337834+IsitaRex@users.noreply.github.com>
2022-11-05 18:33:42 -0500

    Create multiplier_model.py

---
+++ cooper/multipliers/multiplier_model.py
@@ -0,0 +1,43 @@
+
+import torch
+import abc 
+from cooper.multipliers import BaseMultiplier
+
+class MultiplierModel(BaseMultiplier, meta=abc.ABCMeta):
+    """
+    A multiplier model. Holds a :py:class:`~torch.nn.Module`, which predicts
+    the value of the Lagrange multipliers associated with the equality or
+    inequality constraints of a
+    :py:class:`~cooper.problem.ConstrainedMinimizationProblem`.
+
+    Args:
+        model: A :py:class:`~torch.nn.Module` which predicts the values of the
+            Lagrange multipliers.
+        is_positive: Whether to enforce non-negativity on the values of the
+            multiplier.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    @property
+    def grad(self):
+        """Yields the current gradients stored in each fo the model parameters."""
+        for param in self.model.parameters():
+            if param.grad is not None:
+                yield param.grad
+
+    @abc.abstractmethod
+    def forward(self, constraint_features: torch.Tensor):
+        """
+        Returns the *actual* value of the multipliers by
+        passing the "features" of the constraint to predict the corresponding
+        multiplier.
+        """
+        pass
+
+    def __str__(self):
+        return str(self.model.input.data)
+
+    def __repr__(self):
+        pos_str = "inequality" if self.positive else "equality"
