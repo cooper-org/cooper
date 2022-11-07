1faa09781c6a2c24061a1d56655e353370a12f0f
IsitaRex <41337834+IsitaRex@users.noreply.github.com>
2022-11-06 23:56:01 -0500

    Update test_lagrangian_model.py

--- tests/test_lagrangian_model.py
+++ tests/test_lagrangian_model.py
@@ -8,23 +8,25 @@ import cooper
 
 
 def test_lagrangian_model():
-    class DummyCMP(cooper.ConstrainedMinimizationProblem):
-        def __init__(self):
-            super().__init__()
+    
+    # create a model
+    ineq_model = torch.nn.Linear(10, 1)
+    eq_model = torch.nn.Linear(10, 1)
+    # create a multiplier model
+    ineq_multiplier_model = cooper.multipliers.MultiplierModel(ineq_model)
+    eq_multiplier_model = cooper.multipliers.MultiplierModel(eq_model)
+    lf = cooper.formulation.LagrangianModelFormulation(ineq_multiplier_model , eq_multiplier_model )
+    # test lf state
+    eq_featurs = torch.randn(100, 10)
+    ineq_featurs = torch.randn(100, 10)
+    ineq_state, eq_state = lf.state(ineq_featurs, eq_featurs)
 
-        def closure(self):
-            pass
+    assert ineq_state is not None
+    assert eq_state is not None
 
-    cmp = DummyCMP()
-
-    lf = cooper.LagrangianFormulation(cmp)
-    cmp.state = cooper.CMPState(eq_defect=torch.tensor([1.0]))
-    lf.create_state(cmp.state)
-    assert (lf.ineq_multipliers is None) and (lf.eq_multipliers is not None)
-
-    lf = cooper.LagrangianFormulation(cmp)
-    cmp.state = cooper.CMPState(
-        eq_defect=torch.tensor([1.0]), ineq_defect=torch.tensor([1.0, 1.2])
-    )
-    lf.create_state(cmp.state)
-    assert (lf.ineq_multipliers is not None) and (lf.eq_multipliers is not None)
+    # lf = cooper.LagrangianFormulation(cmp)
+    # cmp.state = cooper.CMPState(
+    #     eq_defect=torch.tensor([1.0]), ineq_defect=torch.tensor([1.0, 1.2])
+    # )
+    # lf.create_state(cmp.state)
+    # assert (lf.ineq_multipliers is not None) and (lf.eq_multipliers is not None)
