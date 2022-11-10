183b955f18b73191e2f9b25211a5cfdf8c89c555
daoterog <doterog320@gmail.com>
2022-11-10 18:45:42 -0500

    Rewrite tests for current multiplier model object

--- tests/test_multiplier_model.py
+++ tests/test_multiplier_model.py
@@ -1,26 +1,26 @@
 # create class that inherits from cooper.multipliers.MultiplierModel
 import cooper
 import torch
-import abc
 
+class ToyMultiplierModel(cooper.multipliers.MultiplierModel):
+    """
+    Simplest MultiplierModel possible, a linear model with a single output.
+    """
+
+    def __init__(self, n_params, n_hidden_units, device):
+        super().__init__()
+        self.linear1 = torch.nn.Linear(n_params, n_hidden_units, device=device)
+        self.linear2 = torch.nn.Linear(n_hidden_units, 1, device=device)
+
+    def forward(self, constraint_features: torch.Tensor):
+        x = self.linear1(constraint_features)
+        x = torch.relu(x)
+        x = self.linear2(x)
+        return torch.reshape(torch.nn.functional.relu(x), (-1,))
 
 def test_multiplier_model_init():
-    # create a model
-    model = torch.nn.Linear(10, 1)
-    # create a multiplier model
-    multiplier_model = cooper.multipliers.MultiplierModel(model)
-    # check that the multiplier model is a MultiplierModel
+
+    multiplier_model = ToyMultiplierModel(10, 10, "cpu")
+
     assert isinstance(multiplier_model, cooper.multipliers.BaseMultiplier)
-
-
-def test_multiplier_model_forward():
-    # create a model
-    model = torch.nn.Linear(10, 1)
-    # create a multiplier model
-    multiplier_model = cooper.multipliers.MultiplierModel(model)
-    # create a tensor of constraint features
-    constraint_features = torch.randn(100, 10)
-    # check that the multiplier model is a MultiplierModel
-    assert torch.allclose(
-        multiplier_model(constraint_features), model(constraint_features)
-    )
+    assert isinstance(multiplier_model, cooper.multipliers.MultiplierModel)
