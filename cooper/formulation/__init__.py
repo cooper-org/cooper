26b327e445fd04102a214e04336142a690ead77d
IsitaRex <41337834+IsitaRex@users.noreply.github.com>
2022-12-07 16:53:26 -0500

    Remove MockLagrangianModelFormulation

--- cooper/formulation/__init__.py
+++ cooper/formulation/__init__.py
@@ -2,4 +2,3 @@ from .augmented_lagrangian import AugmentedLagrangianFormulation
 from .formulation import Formulation, UnconstrainedFormulation
 from .lagrangian import LagrangianFormulation, ProxyLagrangianFormulation
 from .lagrangian_model import LagrangianModelFormulation
-from .mock_model_formulation import MockLagrangianModelFormulation
