92143abb17a49ff76d95f8ae204ab316f4c8d7da
daoterog <doterog320@gmail.com>
2023-02-03 12:19:40 -0500

    Simplify object for test

--- tests/test_multiplier_model.py
+++ tests/test_multiplier_model.py
@@ -1,29 +1,15 @@
-# create class that inherits from cooper.multipliers.MultiplierModel
-import torch
-
 import cooper
 
-
-class ToyMultiplierModel(cooper.multipliers.MultiplierModel):
-    """
-    Simplest MultiplierModel possible, a linear model with a single output.
-    """
-
-    def __init__(self, n_params, n_hidden_units, device):
-        super().__init__()
-        self.linear1 = torch.nn.Linear(n_params, n_hidden_units, device=device)
-        self.linear2 = torch.nn.Linear(n_hidden_units, 1, device=device)
-
-    def forward(self, constraint_features: torch.Tensor):
-        x = self.linear1(constraint_features)
-        x = torch.relu(x)
-        x = self.linear2(x)
-        return torch.reshape(torch.nn.functional.relu(x), (-1,))
-
-
 def test_multiplier_model_init():
+    class DummyMultiplierModel(cooper.multipliers.MultiplierModel):
 
-    multiplier_model = ToyMultiplierModel(10, 10, "cpu")
+        def __init__(self):
+            super().__init__()
+
+        def forward(self):
+            pass
+
+    multiplier_model = DummyMultiplierModel()
 
     assert isinstance(multiplier_model, cooper.multipliers.BaseMultiplier)
     assert isinstance(multiplier_model, cooper.multipliers.MultiplierModel)
