f73e4b98586d1e752cfcd581b1b35c600a34d85c
Jose Gallego-Posada <josegp@meta.com>
2023-02-16 21:10:51 -0500

    Join CMPState and ConstrainedMinimizationProblem

--- cooper/cmp.py
+++ cooper/cmp.py
@@ -1,8 +1,13 @@
+import abc
 from typing import Iterable, List, Optional, Tuple
 
 import torch
 
 from cooper.constraints import ConstraintGroup, ConstraintState
+
+# Formulation, and some other classes below, are heavily inspired by the design
+# of the TensorFlow Constrained Optimization (TFCO) library :
+# https://github.com/google-research/tensorflow_constrained_optimization
 
 
 class CMPState:
@@ -21,7 +26,7 @@ class CMPState:
     def __init__(
         self,
         loss: Optional[torch.Tensor] = None,
-        observed_constraints: Iterable[Tuple[ConstraintGroup, Optional[ConstraintState]]] = None,
+        observed_constraints: Optional[Iterable[Tuple[ConstraintGroup, Optional[ConstraintState]]]] = (),
         misc: Optional[dict] = None,
     ):
 
@@ -57,8 +62,15 @@ class CMPState:
 
         for constraint_tuple in self.observed_constraints:
 
-            constraint_group = constraint_tuple[0]
-            constraint_state = constraint_tuple[1] if len(constraint_tuple) == 2 else constraint_group.state
+            if isinstance(constraint_tuple, ConstraintGroup):
+                constraint_group = constraint_tuple
+                constraint_state = constraint_group.state
+            elif isinstance(constraint_tuple, tuple) and len(constraint_tuple) == 2:
+                constraint_group, constraint_state = constraint_tuple
+            else:
+                raise ValueError(
+                    f"Received invalid format for observed constraint. Expected {ConstraintGroup} or {Tuple[ConstraintGroup, ConstraintState]}, but received {type(constraint_tuple)}"
+                )
 
             multiplier_value, primal_contribution, dual_contribution = constraint_group.compute_lagrangian_contribution(
                 constraint_state=constraint_state
@@ -94,3 +106,59 @@ class CMPState:
         # Populate the gradient of the Lagrangian w.r.t. the dual parameters
         if self._dual_lagrangian is not None and isinstance(self._dual_lagrangian, torch.Tensor):
             self._dual_lagrangian.backward()
+
+
+class ConstrainedMinimizationProblem(abc.ABC):
+    """Base class for constrained minimization problems."""
+
+    def __init__(self):
+        self._state = CMPState()
+
+    @property
+    def state(self) -> CMPState:
+        return self._state
+
+    @state.setter
+    def state(self, value: CMPState):
+        self._state = value
+
+    @abc.abstractmethod
+    def closure(self, *args, **kwargs) -> CMPState:
+        """
+        Computes the state of the CMP based on the current value of the primal
+        parameters.
+
+        The signature of this abstract function may be changed to accommodate
+        situations that require a model, (mini-batched) inputs/targets, or
+        other arguments to be passed.
+
+        Structuring the CMP class around this closure method, enables the re-use
+        of shared sections of a computational graph. For example, consider a
+        case where we want to minimize a model's cross entropy loss subject to
+        a constraint on the entropy of its predictions. Both of these quantities
+        depend on the predicted logits (on a minibatch). This closure-centric
+        design allows flexible problem specifications while avoiding
+        re-computation.
+        """
+
+    def defect_fn(self) -> CMPState:
+        """
+        Computes the constraints of the CMP based on the current value of the
+        primal parameters. This function returns a
+        :py:class:`cooper.problem.CMPState` collecting the values of the (proxy
+        and/or non-proxy) constraints. Note that this returned ``CMPState`` may
+        have a ``loss`` attribute with value ``None`` since, by design, the loss
+        is not necessarily computed when evaluating `only` the constraints.
+
+        The signature of this "abstract" function may be changed to accommodate
+        situations that require a model, (mini-batched) inputs/targets, or
+        other arguments to be passed.
+
+        Depending on the problem at hand, the computation of the constraints
+        can be compartimentalized in a way that is independent of the evaluation
+        of the loss. Alternatively,
+        :py:meth:`~.ConstrainedMinimizationProblem.defect_fn` may be used in the
+        implementation of the :py:meth:`~.ConstrainedMinimizationProblem.closure`
+        function.
+        """
+        raise NotImplementedError
