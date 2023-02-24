d1840bd452055d8041ab1e8d8ef4b8b7a46f60af
Jose Gallego-Posada <josegp@meta.com>
2023-02-24 15:12:57 -0500

    Improve docs for CMP and CMPState

--- cooper/cmp.py
+++ cooper/cmp.py
@@ -5,28 +5,33 @@ import torch
 
 from cooper.constraints import ConstraintGroup, ConstraintState
 
-# Formulation, and some other classes below, are heavily inspired by the design
-# of the TensorFlow Constrained Optimization (TFCO) library :
+# Formulation, and some other classes below, are heavily inspired by the design of the
+# TensorFlow Constrained Optimization (TFCO) library:
 # https://github.com/google-research/tensorflow_constrained_optimization
 
 
 class CMPState:
-    """Represents the "state" of a Constrained Minimization Problem in terms of
-    the value of its loss and constraint violations/defects.
+    """Represents the "state" of a Constrained Minimization Problem in terms of the
+    value of its loss and constraint violations/defects.
 
     Args:
         loss: Value of the loss or main objective to be minimized :math:`f(x)`
-        observed_constraints: List of tuples containing the observed/measured
-            constraint groups along with their states. The constraint state may be
-            held internally by the constraint group (`constraint_group.state`), or it
-            may be passed explicitly as the second element of the tuple.
-        misc: Optional additional information to be store along with the state of the CMP
+        observed_constraints: List of tuples containing the observed/measured constraint
+            groups along with their states. The constraint state may be held internally
+            by the constraint group (`constraint_group.state`), or it may be passed
+            explicitly as the second element of the tuple.
+        misc: Optional storage space for additional information relevant to the state of
+            the CMP. This dict enables persisting the results of certain computations
+            for post-processing. For example, one may want to retain the value of the
+            predictions/logits computed over a given minibatch during the call to
+            :py:meth:`~.ConstrainedMinimizationProblem.compute_cmp_state` to measure or
+            log training statistics.
     """
 
     def __init__(
         self,
         loss: Optional[torch.Tensor] = None,
-        observed_constraints: Optional[Iterable[Tuple[ConstraintGroup, Optional[ConstraintState]]]] = (),
+        observed_constraints: Iterable[Tuple[ConstraintGroup, Optional[ConstraintState]]] = (),
         misc: Optional[dict] = None,
     ):
         self.loss = loss
@@ -42,15 +47,21 @@ class CMPState:
         """
         Computes and accumulates the Lagrangian based on the loss and the contributions
         to the "primal" and "dual" Lagrangians resulting from each of the observed
-        constraints. Recall that the Lagrangian contributions correspond to disjoint
-        computational graphs from the point of view of gradient propagation: there is no
-        gradient connection between the primal (resp. dual) Lagrangian contribution and
-        the dual (resp. primal) variables.
+        constraints.
+        The Lagrangian contributions correspond to disjoint computational graphs from
+        the point of view of gradient propagation: there is no gradient connection
+        between the primal (resp. dual) Lagrangian contribution and the dual (resp.
+        primal) variables.
 
         Args:
             return_multipliers: When `True`, we return the value of the multipliers for
                 the observed constraints.
 
+        Returns:
+            primal_lagrangian: Value of the Lagrangian. This tensor has gradient with
+                respect to the primal variables.
+            observed_multiplier_values: When `return_multipliers=True`, return the value
+                of the multiplier for each of the observed_constraints.
         """
 
         primal_lagrangian = self.loss
@@ -66,9 +77,9 @@ class CMPState:
             elif isinstance(constraint_tuple, tuple) and len(constraint_tuple) == 2:
                 constraint_group, constraint_state = constraint_tuple
             else:
-                raise ValueError(
-                    f"Received invalid format for observed constraint. Expected {ConstraintGroup} or {Tuple[ConstraintGroup, ConstraintState]}, but received {type(constraint_tuple)}"
-                )
+                error_message = f"Received invalid format for observed constraint. Expected {ConstraintGroup} or"
+                error_message += f" {Tuple[ConstraintGroup, ConstraintState]}, but received {type(constraint_tuple)}"
+                raise ValueError(error_message)
 
             multiplier_value, primal_contribution, dual_contribution = constraint_group.compute_lagrangian_contribution(
                 constraint_state=constraint_state
@@ -92,20 +103,20 @@ class CMPState:
             return self._primal_lagrangian
 
     def primal_backward(self) -> None:
-        """Populates the gradient of the Lagrangian with respect to the primal parameters."""
+        """Triggers backward calls to compute the gradient of the Lagrangian with
+        respect to the primal variables."""
         if self._primal_lagrangian is not None and isinstance(self._primal_lagrangian, torch.Tensor):
             self._primal_lagrangian.backward()
 
     def dual_backward(self) -> None:
-        """Populates the gradient of the Lagrangian with respect to the dual parameters."""
+        """Triggers backward calls to compute the gradient of the Lagrangian with
+        respect to the dual variables."""
         if self._dual_lagrangian is not None and isinstance(self._dual_lagrangian, torch.Tensor):
             self._dual_lagrangian.backward()
 
     def backward(self) -> None:
-        """
-        Populates the gradient of the Lagrangian with respect to the primal *and* dual
-        parameters.
-        """
+        """Computes the gradient of the Lagrangian with respect to both the primal and
+        dual parameters."""
         self.primal_backward()
         self.dual_backward()
 
@@ -125,44 +136,40 @@ class ConstrainedMinimizationProblem(abc.ABC):
         self._state = value
 
     @abc.abstractmethod
-    def closure(self, *args, **kwargs) -> CMPState:
-        # TODO(juan43ramirez): refactor closure to compute_cmp_state
+    def compute_cmp_state(self, *args, **kwargs) -> CMPState:
         """
         Computes the state of the CMP based on the current value of the primal
         parameters.
 
-        The signature of this abstract function may be changed to accommodate
-        situations that require a model, (mini-batched) inputs/targets, or
-        other arguments to be passed.
+        The signature of this abstract function may be changed to accommodate situations
+        that require a model, (mini-batched) inputs/targets, or other arguments to be
+        passed.
 
-        Structuring the CMP class around this closure method, enables the re-use
-        of shared sections of a computational graph. For example, consider a
-        case where we want to minimize a model's cross entropy loss subject to
-        a constraint on the entropy of its predictions. Both of these quantities
-        depend on the predicted logits (on a minibatch). This closure-centric
-        design allows flexible problem specifications while avoiding
-        re-computation.
+        Structuring the CMP class around this method, enables the re-use of shared
+        sections of a computational graph. For example, consider a case where we want to
+        minimize a model's cross entropy loss subject to a constraint on the entropy of
+        its predictions. Both of these quantities depend on the predicted logits (on a
+        minibatch). This closure-centric design allows flexible problem specifications
+        while avoiding re-computation.
         """
 
-    def defect_fn(self) -> CMPState:
-        # TODO(juan43ramirez): refactor defect_fn to compute_violations
+    def compute_violations(self) -> CMPState:
         """
-        Computes the constraints of the CMP based on the current value of the
-        primal parameters. This function returns a
-        :py:class:`cooper.problem.CMPState` collecting the values of the (proxy
-        and/or non-proxy) constraints. Note that this returned ``CMPState`` may
-        have a ``loss`` attribute with value ``None`` since, by design, the loss
-        is not necessarily computed when evaluating `only` the constraints.
+        Computes the violation of (a subset of) the constraints of the CMP based on the
+        current value of the primal parameters. This function returns a
+        :py:class:`cooper.problem.CMPState` collecting the values of the observed
+        constraints. Note that the returned ``CMPState`` may have ``loss=None`` since,
+        by design, the value of the loss is not necessarily computed when evaluating
+        `only` the constraints.
 
         The signature of this "abstract" function may be changed to accommodate
-        situations that require a model, (mini-batched) inputs/targets, or
-        other arguments to be passed.
+        situations that require a model, (mini-batched) inputs/targets, or other
+        arguments to be passed.
 
-        Depending on the problem at hand, the computation of the constraints
-        can be compartimentalized in a way that is independent of the evaluation
-        of the loss. Alternatively,
-        :py:meth:`~.ConstrainedMinimizationProblem.defect_fn` may be used in the
-        implementation of the :py:meth:`~.ConstrainedMinimizationProblem.closure`
-        function.
+        Depending on the problem at hand, the computation of the constraints can be
+        compartimentalized in a way that is independent of the evaluation of the loss.
+        Alternatively, :py:meth:`~.ConstrainedMinimizationProblem.compute_violations`
+        may be called during the execution of the
+        :py:meth:`~.ConstrainedMinimizationProblem.compute_cmp_state` method.
         """
         raise NotImplementedError
