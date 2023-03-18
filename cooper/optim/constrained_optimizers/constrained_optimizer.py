5ce13f4a43553f43dfd513cb4abf9f5cb7c42bce
Jose Gallego-Posada <josegp@meta.com>
2023-03-17 22:28:44 -0400

    Update base ConstrainedOptimizer

--- cooper/optim/constrained_optimizers/constrained_optimizer.py
+++ cooper/optim/constrained_optimizers/constrained_optimizer.py
@@ -11,6 +11,7 @@ from typing import Dict, List, Optional, Union
 import torch
 
 from cooper.constraints import ConstraintGroup
+from cooper.multipliers import MULTIPLIER_TYPE
 from cooper.utils import ensure_iterable, validate_state_dicts
 
 
@@ -21,15 +22,16 @@ class CooperOptimizerState:
     the dual scheduler if applicable. This is used for checkpointing.
 
     Args:
-        primal_optimizer_states: State dict for the primal optimizers.
-        dual_optimizer_state: State dict for the dual optimizer.
-        dual_scheduler_state: State dict for the dual scheduler.
+        primal_optimizer_states: State dicts for the primal optimizers.
+        dual_optimizer_states: State dicts for the dual optimizers.
+        multiplier_states: State dicts for the multipliers.
     """
 
     primal_optimizer_states: List[Dict]
     dual_optimizer_states: Optional[List[Dict]] = None
-    extrapolation: bool = False
-    alternating: bool = False
+    multiplier_states: Optional[List[Dict]] = None
+    extrapolation: Optional[bool] = False
+    alternating: Optional[bool] = False
 
     def __eq__(self, other):
         assert isinstance(other, CooperOptimizerState)
@@ -40,7 +42,7 @@ class CooperOptimizerState:
             except:
                 return False
 
-        state_dict_names = ["primal_optimizer_states", "dual_optimizer_states"]
+        state_dict_names = ["primal_optimizer_states", "dual_optimizer_states", "multiplier_states"]
         all_checks = [compare_state_dicts(_) for _ in state_dict_names]
 
         all_checks.append(self.extrapolation == other.extrapolation)
@@ -60,96 +62,75 @@ class ConstrainedOptimizer:
 
     A ``ConstrainedOptimizer`` includes one or more
     :class:`torch.optim.Optimizer`\\s for the primal variables. It also includes
-    a :class:`torch.optim.Optimizer` for the dual variables associated with the
-    provided ``Formulation``.
+    one or more :class:`torch.optim.Optimizer`\\s for the dual variables.
 
-    For handling unconstrained problems, we provide an
-    :py:class:`~cooper.optim.cooper_optimizer.UnconstrainedOptimizer`. Please
-    refer to the documentation of the
-    :py:class:`~cooper.problem.ConstrainedMinimizationProblem` and
+    For handling unconstrained problems in a consistent way, we provide an
+    :py:class:`~cooper.optim.UnconstrainedOptimizer`. Please refer to the documentation
+    of the :py:class:`~cooper.problem.ConstrainedMinimizationProblem` and
     :py:class:`~cooper.formulation.Formulation` classes for further details on
     handling unconstrained problems.
 
     Args:
-        constraint_groups:
+        primal_optimizers: Optimizer(s) for the primal variables (e.g. the weights of
+            a model). The primal parameters can be partitioned into multiple optimizers,
+            in this case ``primal_optimizers`` accepts a list of
+            ``torch.optim.Optimizer``\\s.
 
-        primal_optimizers: Fully instantiated ``torch.optim.Optimizer``\\s used
-            to optimize the primal parameters (e.g. model parameters). The primal
-            parameters can be partitioned into multiple optimizers, in this case
-            ``primal_optimizers`` accepts a list of ``torch.optim.Optimizer``\\s.
+        dual_optimizers: Optimizer(s) for the dual variables (e.g. the Lagrange
+            multipliers associated with the constraints). An iterable of
+            ``torch.optim.Optimizer``\\s can be passed to handle the case of several
+            ``~cooper.constraints.ConstraintGroup``\\s. If dealing with an unconstrained
+            problem, please use a
+            :py:class:`~cooper.optim.cooper_optimizer.UnconstrainedOptimizer` instead.
 
-        dual_optimizer: Partially instantiated ``torch.optim.Optimizer``
-            used to optimize the dual variables (e.g. Lagrange multipliers).
-            If dealing with an unconstrained problem, please use a
-            :py:class:`~cooper.optim.cooper_optimizer.UnconstrainedOptimizer`
-            instead.
+        multipliers: Multiplier(s) associated with the constrained optimization problem.
+            We keep a reference to the multipliers to post-process them after the dual
+            optimizer steps. If `multipliers=None` we extract the multipliers from the
+            `constraint_groups`. Exactly one of `multipliers` and `constraint_groups`
+            must be not `None`.
 
-        dual_scheduler: Partially instantiated
-            ``torch.optim.lr_scheduler._LRScheduler`` used to schedule the
-            learning rate of the dual variables. Defaults to None.
-
-        extrapolation: Whether to perform extragradient updates. Defaults to False.
-
-        alternating: Whether to alternate parameter updates between primal and
-            dual parameters. Otherwise, do simultaneous parameter updates.
-            Defaults to False.
-
-        dual_restarts: If True, perform "restarts" on the Lagrange
-            multipliers associated with inequality constraints: whenever the
-            constraint is satisfied, directly set the multiplier to zero.
-            Defaults to False.
+        constraint_groups: Constraint group(s) associated with the constrained
+            optimization problem. We use the constraint groups to extract references to
+            the multipliers. Exactly one of `multipliers` and `constraint_groups` must
+            be not `None`.
 
     """
 
+    extrapolation = None
+    alternating = None
+
     def __init__(
         self,
-        constraint_groups: Union[List[ConstraintGroup], ConstraintGroup],
-        primal_optimizers: Union[List[torch.optim.Optimizer], torch.optim.Optimizer],
-        dual_optimizers: Union[List[torch.optim.Optimizer], torch.optim.Optimizer],
+        primal_optimizers: Union[torch.optim.Optimizer, List[torch.optim.Optimizer]],
+        dual_optimizers: Union[torch.optim.Optimizer, List[torch.optim.Optimizer]],
+        multipliers: Optional[Union[MULTIPLIER_TYPE, List[MULTIPLIER_TYPE]]] = None,
+        constraint_groups: Optional[Union[List[ConstraintGroup], ConstraintGroup]] = None,
     ):
-        self.constraint_groups = ensure_iterable(constraint_groups)
         self.primal_optimizers = ensure_iterable(primal_optimizers)
         self.dual_optimizers = ensure_iterable(dual_optimizers)
+
+        if (constraint_groups is None) ^ (multipliers is None):
+            raise ValueError("Exactly one of `constraint_groups` and `multipliers` must be not None.")
+
+        if multipliers is not None:
+            self.multipliers = ensure_iterable(multipliers)
+        else:
+            self.multipliers = [constraint.multiplier for constraint in ensure_iterable(constraint_groups)]
 
     def base_sanity_checks(self):
         """
         Perform sanity checks on the initialization of ``ConstrainedOptimizer``.
         """
 
-        if len(self.constraint_groups) != len(self.dual_optimizers):
-            raise RuntimeError("One dual optimizer must be provided for each constraint group.")
-
-        if self.constraint_groups is None:
-            raise RuntimeError("No constraints were provided.")
-
         if self.primal_optimizers is None:
-            raise RuntimeError("No primal optimizer was provided.")
-
+            raise RuntimeError("No primal optimizer(s) was provided for building a ConstrainedOptimizer.")
         if self.dual_optimizers is None:
-            raise RuntimeError("No dual optimizer was provided.")
-
-        any_restart_on_feasible = False
-        any_is_augmented_lagrangian = False
-        for constraint in self.constraint_groups:
-            if hasattr(constraint.multiplier.restart_on_feasible) and constraint.multiplier.restart_on_feasible:
-                any_restart_on_feasible = True
-            if constraint.formulation.formulation_type == "augmented_lagrangian":
-                any_is_augmented_lagrangian = True
-
-        if self.alternating and any_restart_on_feasible:
-            warnings.warn(
-                """Using alternating updates with dual restarts is untested.
-                Please use with caution."""
-            )
-
-        if any_is_augmented_lagrangian and not self.alternating:
-            raise RuntimeError("Augmented Lagrangian formulation requires alternating updates.")
+            raise RuntimeError("No dual optimizer(s) was provided for building a ConstrainedOptimizer.")
 
     def zero_grad(self):
         """
-        Sets the gradients of all optimized
-        :py:class:`~torch.nn.parameter.Parameter`\\s to zero. This includes both
-        the primal and dual variables.
+        Sets the gradients of all optimized :py:class:`~torch.nn.parameter.Parameter`\\s
+        to zero. This includes both the primal and dual variables.
         """
         for primal_optimizer in self.primal_optimizers:
             primal_optimizer.zero_grad()
@@ -165,10 +146,12 @@ class ConstrainedOptimizer:
 
         primal_optimizer_states = [_.state_dict() for _ in self.primal_optimizers]
         dual_optimizer_states = [_.state_dict() for _ in self.dual_optimizers]
+        multiplier_states = [_.state_dict() for _ in self.multipliers]
 
         return CooperOptimizerState(
             primal_optimizer_states=primal_optimizer_states,
             dual_optimizer_states=dual_optimizer_states,
+            multiplier_states=multiplier_states,
             extrapolation=self.extrapolation,
             alternating=self.alternating,
         )
