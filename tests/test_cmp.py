009f38f2276effd1fc0f17aeec99da1bf375bd13
Jose Gallego-Posada <josegp@meta.com>
2023-03-19 23:18:08 -0400

    Update basic CMP test

--- tests/test_cmp.py
+++ tests/test_cmp.py
@@ -1,18 +1,33 @@
+import cooper_test_utils
 import pytest
+import testing_utils
 import torch
 
 import cooper
-from tests.helpers.cooper_test_utils import Toy2dCMP
 
 
-def test_cmp():
+def test_cmp(device):
 
-    params = torch.nn.Parameter(torch.tensor([0.1, 0.5]))
-    cmp = Toy2dCMP(use_ineq_constraints=True)
+    params = torch.nn.Parameter(torch.tensor([0.1, 0.5], device=device))
+    cmp = cooper_test_utils.Toy2dCMP(use_ineq_constraints=True, device=device)
 
-    cmp_state = cmp.closure(params)
+    primal_optimizer = torch.optim.SGD([params], lr=1e-2)
 
-    lagrangian, multipliers = cmp_state.populate_lagrangian(return_multipliers=True)
-    cmp_state.backward()
+    dual_params = [{"params": constraint.multiplier.parameters()} for constraint in cmp.constraint_groups]
+    dual_optimizer = torch.optim.SGD(dual_params, lr=1e-2)
 
-    breakpoint()
+    constrained_optimizer = cooper.optim.SimultaneousConstrainedOptimizer(
+        primal_optimizers=primal_optimizer, dual_optimizers=dual_optimizer, constraint_groups=cmp.constraint_groups
+    )
+
+    for step_id in range(1000):
+
+        constrained_optimizer.zero_grad()
+        cmp_state = cmp.compute_cmp_state(params)
+        lagrangian, multipliers = cmp_state.populate_lagrangian(return_multipliers=True)
+        cmp_state.backward()
+        constrained_optimizer.step()
+
+    # Constrained optimum is located at [2/3, 1/3]
+    assert torch.allclose(params[0], torch.tensor(2.0 / 3.0))
+    assert torch.allclose(params[1], torch.tensor(1.0 / 3.0))
