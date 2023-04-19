aa5fa67dcdf97c2ed374e3659eac0209cafe920a
daoterog <doterog320@gmail.com>
2023-04-19 17:32:08 -0500

    Last version of least squares tutorial

--- tutorials/scripts/plot_least_squares.py
+++ tutorials/scripts/plot_least_squares.py
@@ -1,10 +1,8 @@
-from copy import deepcopy
-from typing import Tuple
+from typing import Tuple, Dict, Any, Union, List
 
 import random
 
 import cooper
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
@@ -14,8 +12,8 @@ from cooper.optim import SimultaneousConstrainedOptimizer
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.sampler import RandomSampler, BatchSampler
 
+import matplotlib.pyplot as plt
 
-torch.manual_seed(0)
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -39,11 +37,43 @@ class LeastSquares(cooper.ConstrainedMinimizationProblem):
 
     def compute_cmp_state(self, x: torch.Tensor, A: torch.Tensor, b: torch.Tensor, indices: torch.Tensor) -> CMPState:
 
+        # FIX: consitency between functions
         loss = torch.linalg.vector_norm(x) ** 2
-        violations = torch.matmul(A, x) - b
+        violations = A @ x - b
         self.eq_group.state = ConstraintState(violation=violations, constraint_features=indices)
 
         return CMPState(loss=loss, observed_constraints=[self.eq_group])
+
+
+def create_linear_system(n_eqs: int, n_vars: int):
+
+    # Set seed for reproducibility
+    torch.manual_seed(0)
+
+    # Create random matrix
+    initial_A = torch.randn(n_eqs, n_vars, device=DEVICE)
+    U, _, V = torch.linalg.svd(initial_A)
+
+    new_s = torch.zeros(n_eqs, n_vars, device=DEVICE)
+    for i in range(min(n_eqs, n_vars)):
+        new_s[i, i] = 1
+
+    # Create new matrix with condition number 1
+    A = torch.mm(U, torch.mm(new_s, V.T))
+
+    # Create random vector
+    dummy_x = torch.randn(n_vars, 1, device=DEVICE)
+
+    # Generate b with x to guarantee a solution
+    b = torch.mm(A, dummy_x)
+
+    # Find optimal least squares solution using Pytorch
+    x_optim = torch.linalg.lstsq(A, b).solution
+
+    # Check if the solution is correct
+    assert torch.allclose(torch.mm(A, x_optim), b, atol=1e-5)
+
+    return A, b, x_optim
 
 
 def seed_worker(worker_id):
@@ -52,109 +82,212 @@ def seed_worker(worker_id):
     random.seed(worker_seed)
 
 
-n_vars = 4
-n_eqs = 4
-batch_size = 4
-primal_lr = 1e-3
-dual_lr = 1e-7
-n_epochs = 1000
+def instantiate_dataloader(A: torch.Tensor, b: torch.Tensor, batch_size: int) -> DataLoader:
 
-# Randomly generate a linear system and create a dataset with it
-A = torch.rand(size=(n_eqs, n_vars), device=DEVICE)
-b = torch.rand(size=(n_eqs, 1), device=DEVICE)
-linear_system = LinearConstraintSystem(A, b)
+    # Instantiate Dataset
+    linear_system = LinearConstraintSystem(A, b)
 
-# Find the optimal solution by solving the linear system with PyTorch
-x_optim = torch.linalg.lstsq(A, b).solution
+    # Create a dataloader that samples uniformly with replacement. Pass a generator and a
+    # worker_init_fn to ensure reproducibility
+    g = torch.Generator()
+    g.manual_seed(0)
 
-# Check that the solution is correct
-assert torch.allclose(torch.matmul(A, x_optim), b, atol=1e-5)
+    # Create a random sampler and a batch sampler to sample the constraints uniformly with replacement
+    random_sampler = RandomSampler(linear_system, replacement=True, generator=g)
+    batch_sampler = BatchSampler(random_sampler, batch_size=batch_size, drop_last=False)
 
-# Create a dataloader that samples uniformly with replacement. Pass a generator and a
-# worker_init_fn to ensure reproducibility
-g = torch.Generator()
-g.manual_seed(0)
+    # Create the dataloader
+    dataloader_kwargs = {
+        "sampler": batch_sampler,
+        "worker_init_fn": seed_worker,
+        "batch_size": None,  # Set batch_size to None to avoid additional dimension in the
+        # output of the dataloader. The batch size is already specified
+        # in the sampler so there is no need to specify it again.
+    }
 
-# Create a random sampler and a batch sampler to sample the constraints uniformly with replacement
-random_sampler = RandomSampler(linear_system, replacement=True)  # , generator=g)
-batch_sampler = BatchSampler(random_sampler, batch_size=batch_size, drop_last=False)
+    return DataLoader(linear_system, **dataloader_kwargs)
 
-# Create the dataloader
-dataloader_kwargs = {
-    "sampler": batch_sampler,
-    "worker_init_fn": seed_worker,
-    "batch_size": None,  # Set batch_size to None to avoid additional dimension in the
-    # output of the dataloader. The batch size is already specified
-    # in the sampler so there is no need to specify it again.
-}
-constraint_loader = DataLoader(linear_system, **dataloader_kwargs)
 
-# Create a constraint group for the equality constraints. We use a sparse constraint
-# to be able to update the multipliers only with the observed constraints (i.e. the
-# ones that are active in the current batch)
-eq_group = ConstraintGroup(constraint_type="eq", shape=A.shape[0], dtype=torch.float32, device=DEVICE, is_sparse=True)
+def instantiate_cooper_variables(n_eqs: int, n_vars: int, primal_lr: float, dual_lr: float):
 
-# Define the problem with the constraint group
-cmp = LeastSquares(eq_group=eq_group)
+    # Create a constraint group for the equality constraints. We use a sparse constraint
+    # to be able to update the multipliers only with the observed constraints (i.e. the
+    # ones that are active in the current batch)
+    eq_group = ConstraintGroup(constraint_type="eq", shape=n_eqs, dtype=torch.float32, device=DEVICE, is_sparse=True)
 
-# Randomly initialize the primal variable and instantiate the optimizers
-x = torch.nn.Parameter(torch.rand(n_vars, 1, device=DEVICE))
+    # Define the problem with the constraint group
+    cmp = LeastSquares(eq_group=eq_group)
 
-primal_optimizer = torch.optim.SGD([x], lr=primal_lr, momentum=0.7)
-dual_optimizer = torch.optim.SGD(eq_group.multiplier.parameters(), lr=dual_lr)  # , momentum=0.7)
+    # Randomly initialize the primal variable and instantiate the optimizers
+    x = torch.nn.Parameter(torch.rand(n_vars, 1, device=DEVICE))
 
-optimizer = SimultaneousConstrainedOptimizer(
-    constraint_groups=eq_group,
-    primal_optimizers=primal_optimizer,
-    dual_optimizers=dual_optimizer,
-)
+    primal_optimizer = torch.optim.SGD([x], lr=primal_lr)  # , momentum=0.7)
+    dual_optimizer = torch.optim.SGD(eq_group.multiplier.parameters(), lr=dual_lr)  # , momentum=0.7)
 
-# Run the optimization process
-state_history = {}
-for i in range(n_epochs):
+    optimizer = SimultaneousConstrainedOptimizer(
+        constraint_groups=eq_group,
+        primal_optimizers=primal_optimizer,
+        dual_optimizers=dual_optimizer,
+    )
 
-    # Create empty tensor to accumulate the violation of the observed constraints
-    acumulated_violation = torch.zeros_like(b)
+    return eq_group, cmp, x, optimizer
 
-    for sampled_A, sampled_b, indices in constraint_loader:
-        optimizer.zero_grad()
-        cmp_state = cmp.compute_cmp_state(x, sampled_A, sampled_b, indices)
-        _ = cmp_state.populate_lagrangian()
-        cmp_state.backward()
-        optimizer.step()
 
-        # Accumulate the violation of the observed constraints
-        acumulated_violation[indices] += cmp_state.observed_constraints[0].state.violation[indices]
+def _to_cpu_numpy(
+    x: Union[torch.Tensor, list, dict[str, torch.Tensor]]
+) -> Union[np.ndarray, list, dict[str, np.ndarray]]:
 
-    if i % 20 == 0:
-        state_history[i] = {
-            "loss": cmp_state.loss.item(),
-            "multipliers": deepcopy(eq_group.multiplier.weight.detach().data),
-            "x": deepcopy(x.detach().data),
-            "x_dif": deepcopy((x - x_optim).detach().data),
-            "violation": acumulated_violation.detach().data,
+    if isinstance(x, torch.Tensor):
+        return x.detach().cpu().numpy()
+    elif isinstance(x, list):
+        return [_to_cpu_numpy(v) for v in x]
+    elif isinstance(x, dict):
+        return {k: _to_cpu_numpy(v) for k, v in x.items()}
+    else:
+        raise ValueError(f"Unknown type: {type(x)}")
+
+
+def solve_least_squares(
+    constraint_loader: DataLoader,
+    eq_group: ConstraintGroup,
+    cmp: cooper.ConstrainedMinimizationProblem,
+    x: torch.Tensor,
+    optimizer: SimultaneousConstrainedOptimizer,
+    x_optim: torch.Tensor,
+    n_eqs: int,
+    n_epochs: int,
+) -> Dict[int, Dict[str, Union[float, np.ndarray]]]:
+
+    # Optimal norm
+    optimal_norm = x_optim.norm().item() ** 2
+
+    # Run the optimization process
+    state_history = {}
+    for i in range(n_epochs):
+
+        # Create empty tensor to accumulate the violation of the observed constraints
+        acumulated_violation = torch.zeros(n_eqs, 1, device=DEVICE)
+
+        for sampled_A, sampled_b, indices in constraint_loader:
+            optimizer.zero_grad()
+            cmp_state = cmp.compute_cmp_state(x, sampled_A, sampled_b, indices)
+            _ = cmp_state.populate_lagrangian()
+            cmp_state.backward()
+            optimizer.step()
+
+            # Accumulate the violation of the observed constraints
+            acumulated_violation[indices] = cmp_state.observed_constraints[0].state.violation
+
+        if i % 20 == 0:
+            state_history[i] = {
+                "loss": cmp_state.loss.item() / optimal_norm,
+                "multipliers": _to_cpu_numpy(eq_group.multiplier.weight),
+                "x_dif": _to_cpu_numpy(x - x_optim),
+                "violation": _to_cpu_numpy(acumulated_violation),
+            }
+
+    return state_history
+
+
+def pipeline(config: Dict[str, Any]):
+
+    # Create a random linear system
+    A, b, x_optim = create_linear_system(
+        n_eqs=config["n_eqs"],
+        n_vars=config["n_vars"],
+    )
+
+    # Instantiate the dataloader
+    constraint_loader = instantiate_dataloader(
+        A=A,
+        b=b,
+        batch_size=config["batch_size"],
+    )
+
+    # Instantiate the Cooper variables
+    eq_group, cmp, x, optimizer = instantiate_cooper_variables(
+        n_eqs=config["n_eqs"],
+        n_vars=config["n_vars"],
+        primal_lr=config["primal_lr"],
+        dual_lr=config["dual_lr"],
+    )
+
+    # Run the optimization process
+    state_history = solve_least_squares(
+        constraint_loader=constraint_loader,
+        eq_group=eq_group,
+        cmp=cmp,
+        x=x,
+        optimizer=optimizer,
+        x_optim=x_optim,
+        n_eqs=config["n_eqs"],
+        n_epochs=config["n_epochs"],
+    )
+
+    return state_history
+
+
+def _stack_squeeze(x: np.ndarray) -> np.ndarray:
+    return np.stack(x).squeeze()
+
+
+def plot_results(
+    state_history_list: List[Dict[int, Dict[str, Union[float, np.ndarray]]]], experiment_ids: List[str]
+) -> None:
+
+    _, ax = plt.subplots(1, 4, figsize=(20, 4))
+
+    for state_history, experiment_id in zip(state_history_list, experiment_ids):
+
+        # Plot the results
+        iters, loss_hist, multipliers_hist, x_dif_hist, violation_hist = zip(
+            *[(k, v["loss"], v["multipliers"], v["x_dif"], v["violation"]) for k, v in state_history.items()]
+        )
+
+        ax[0].plot(iters, loss_hist, label=experiment_id)
+        ax[0].set_title("loss")
+
+        ax[1].plot(iters, _stack_squeeze(multipliers_hist))
+        ax[1].set_title("multipliers")
+
+        ax[2].plot(iters, _stack_squeeze(x_dif_hist).squeeze())
+        ax[2].set_title("x_diff")
+        ax[2].axhline(0, color="red", linestyle="--", alpha=0.5)
+
+        ax[3].plot(iters, _stack_squeeze(violation_hist))
+        ax[3].set_title("violation")
+        ax[3].axhline(0, color="red", linestyle="--", alpha=0.5)
+
+    ax[0].legend(loc="lower right")
+
+    plt.show()
+
+
+n_experiments = 10
+experiment_counter = 1
+n_eqs = 50
+quarter = n_eqs // 4
+
+primal_lr = 5e-3
+dual_lr = 5e-3
+
+state_history_list = []
+experiment_ids = []
+for n_vars in [25, 37, 50, 62, 75]:
+    for batch_size in [25, 50]:
+        config = {
+            "n_eqs": n_eqs,
+            "n_vars": n_vars,
+            "batch_size": batch_size,
+            "primal_lr": primal_lr,
+            "dual_lr": dual_lr,
+            "n_epochs": 10000,
         }
+        print(f"Experiment {experiment_counter}/{n_experiments}: {config}")
+        experiment_counter += 1
 
-    if i % 100 == 0:
-        print(f"Epoch {i} - loss: {cmp_state.loss.item():.4f}")
+        state_history = pipeline(config)
+        state_history_list.append(state_history)
+        experiment_ids.append(f"n_vars={n_vars}_batch_size={batch_size}")
 
-
-# Plot the results
-iters, loss_hist, multipliers_hist, x_hist, x_dif_hist, violation_hist = zip(
-    *[(k, v["loss"], v["multipliers"], v["x"], v["x_dif"], v["violation"]) for k, v in state_history.items()]
-)
-fig, ax = plt.subplots(1, 5, figsize=(20, 4), sharex=True)
-
-hist_list = [multipliers_hist, x_hist, x_dif_hist, violation_hist]
-hist_names = ["multipliers", "x", "x_dif", "violation"]
-
-ax[0].plot(iters, loss_hist)
-ax[0].set_title("loss")
-
-for ax, hist, title in zip(ax[1:], hist_list, hist_names):
-    ax.plot(iters, torch.stack(hist).squeeze().cpu())
-    print(f"PLOT {title}")
-    ax.set_title(title)
-
-plt.savefig("least_squares.png")
-# plt.show()
+plot_results(state_history_list, experiment_ids)
