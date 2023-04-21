6098f5f0dcfc03bc111f30d75b3c38fa3f26d9aa
juan43ramirez <juan43.ramirez@gmail.com>
2023-04-21 13:31:22 -0400

    Update plot_max_entropy tutorial
    Co-authored-by: Jose Gallego-Posada <gallego-posada@users.noreply.github.com>
    Co-authored-by: Daniel Otero Gómez <daoterog@users.noreply.github.com>

--- tutorials/scripts/plot_max_entropy.py
+++ tutorials/scripts/plot_max_entropy.py
@@ -11,8 +11,6 @@ whose average roll is known to be 4.5, what is the maximum entropy distribution
 for the faces?*
 """
 
-import copy
-import os
 from copy import deepcopy
 
 import matplotlib.pyplot as plt
@@ -21,9 +19,7 @@ import torch
 from style_utils import *
 
 import cooper
-
 from cooper import CMPState, ConstraintGroup, ConstraintState
-from cooper.optim import SimultaneousConstrainedOptimizer
 
 torch.manual_seed(0)
 np.random.seed(0)
@@ -32,54 +28,43 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class MaximumEntropy(cooper.ConstrainedMinimizationProblem):
-    def __init__(self, mean_constraint: float, ineq_group: ConstraintGroup, eq_group: ConstraintGroup) -> None:
-        self.mean_constraint = mean_constraint
-        self.ineq_group = ineq_group
-        self.eq_group = eq_group
+    def __init__(self, target_mean: float) -> None:
+        self.target_mean = target_mean
+        self.mean_constraint = ConstraintGroup(constraint_type="eq", shape=1, device=DEVICE)
+        self.sum_constraint = ConstraintGroup(constraint_type="eq", shape=1, device=DEVICE)
+        self.all_constraints = [self.sum_constraint, self.mean_constraint]
+
         super().__init__()
 
-    def compute_cmp_state(self, probs: torch.Tensor) -> CMPState:
-        # Verify domain of definition of the functions
-        assert torch.all(probs >= 0)
-
+    def compute_cmp_state(self, log_probs: torch.Tensor) -> CMPState:
+        probs = torch.exp(log_probs)
         # Loose negative sign since we want to *maximize* the entropy
-        entropy = torch.sum(probs * torch.log(probs))
-
-        # Entries of p >= 0 (equiv. to -p <= 0)
-        self.ineq_group.state = ConstraintState(violation=-probs)
+        entropy = torch.sum(probs * log_probs)
 
         # Equality constraints for proper normalization and mean constraint
-        mean = torch.sum(torch.tensor(range(1, len(probs) + 1)) * probs)
-        self.eq_group.state = ConstraintState(
-            violation=torch.stack([torch.sum(probs) - 1, mean - self.mean_constraint])
-        )
+        mean = torch.sum(probs * torch.arange(1, len(probs) + 1, device=DEVICE))
 
-        return CMPState(loss=entropy, observed_constraints=[self.ineq_group, self.eq_group])
+        self.sum_constraint.state = ConstraintState(violation=torch.sum(probs) - 1)
+        self.mean_constraint.state = ConstraintState(violation=mean - self.target_mean)
 
+        return CMPState(loss=entropy, observed_constraints=self.all_constraints)
 
-# Define the constraint groups
-ineq_group = ConstraintGroup(constraint_type="ineq", shape=6, dtype=torch.float16, device=DEVICE)
-eq_group = ConstraintGroup(constraint_type="eq", shape=2, dtype=torch.float16, device=DEVICE)
 
 # Define the problem with the constraint groups
-cmp = MaximumEntropy(mean_constraint=4.5, ineq_group=ineq_group, eq_group=eq_group)
+cmp = MaximumEntropy(target_mean=4.5)
 
 # Define the primal parameters and optimizer
-rand_init = torch.rand(6, device=DEVICE)  # Use a 6-sided die
-probs = torch.nn.Parameter(rand_init / sum(rand_init))
-primal_optimizer = torch.optim.SGD([probs], lr=3e-2, momentum=0.7)
-
-# TODO(gallego-posada): The tutorial is originally implemented with ExtraSGD, but it is not
-# implemented yet
+log_probs = torch.nn.Parameter(torch.log(torch.ones(6, device=DEVICE) / 6))
+primal_optimizer = cooper.optim.ExtraSGD([log_probs], lr=3e-2)
 
 # Define the dual optimizer
-dual_optimizer = torch.optim.SGD(
-    [ineq_group.multiplier.parameters(), eq_group.multiplier.parameters()], lr=1e-3, momentum=0.4
-)
+dual_parameters = []
+[dual_parameters.extend(_.multiplier.parameters()) for _ in cmp.all_constraints]
+dual_optimizer = cooper.optim.ExtraSGD(dual_parameters, lr=5e-2)
 
-# Wrap the formulation and both optimizers inside a SimultaneousConstrainedOptimizer
-optimizer = SimultaneousConstrainedOptimizer(
-    constraint_groups=eq_group,
+# Wrap the formulation and both optimizers inside a ExtrapolationConstrainedOptimizer
+optimizer = cooper.optim.ExtrapolationConstrainedOptimizer(
+    constraint_groups=cmp.all_constraints,
     primal_optimizers=primal_optimizer,
     dual_optimizers=dual_optimizer,
 )
@@ -87,15 +72,15 @@ optimizer = SimultaneousConstrainedOptimizer(
 state_history = {}
 for i in range(5000):
     optimizer.zero_grad()
-    cmp_state = cmp.compute_cmp_state(probs)
+    cmp_state = cmp.compute_cmp_state(log_probs)
     _ = cmp_state.populate_lagrangian()
     cmp_state.backward()
-    optimizer.step()
+    optimizer.step(compute_cmp_state_fn=lambda: cmp.compute_cmp_state(log_probs))
 
     state_history[i] = {
         "loss": cmp_state.loss.item(),
-        "multipliers": deepcopy(eq_group.multiplier.weight.data),
-        "violation": deepcopy(cmp_state.observed_constraints[1].state.violation.data),
+        "multipliers": deepcopy(torch.stack([_.multiplier().data for _ in cmp.all_constraints])),
+        "violation": deepcopy(torch.stack([_.state.violation.data for _ in cmp.all_constraints])),
     }
 
 # Theoretical solution
@@ -107,10 +92,7 @@ iters, loss_hist, multipliers_hist, violation_hist = zip(
     *[(k, v["loss"], v["multipliers"], v["violation"]) for k, v in state_history.items()]
 )
 
-hist_list = [violation_hist, multipliers_hist]
-hist_names = ["violation", "multipliers"]
-
-_, (ax0, ax1, ax2) = plt.subplots(1, len(hist_list), figsize=(20, 4))
+_, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize=(20, 4))
 
 ax0.plot(iters, torch.stack(multipliers_hist).squeeze().cpu())
 ax0.set_title("Multipliers")
