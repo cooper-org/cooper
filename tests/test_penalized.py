8f880a13eca8163aec7efd208631d83ec4e1f2cc
juan43ramirez <juan43.ramirez@gmail.com>
2023-05-14 15:49:51 -0400

    placeholder test for penalized and QP formulations

--- tests/test_penalized.py
+++ tests/test_penalized.py
@@ -1,1 +1,15 @@
+from cooper import FormulationType
+
 # TODO(juan43ramirez)
+
+
+def test_penalized_formulation():
+    pass
+
+
+def test_quadratic_penalty_formulation():
+    pass
+
+
+def test_save_and_load_state_dict():
+    pass
