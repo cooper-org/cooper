84ee324b09c27a3b22c36e33d4929abb5d390cc1
juan43ramirez <juan43.ramirez@gmail.com>
2023-05-25 14:41:29 -0400

    Add PID to optim init

--- cooper/optim/__init__.py
+++ cooper/optim/__init__.py
@@ -2,5 +2,6 @@ from . import utils
 from .constrained_optimizers import *
 from .extra_optimizers import ExtraAdam, ExtragradientOptimizer, ExtraSGD
 from .optimizer_state import CooperOptimizerState
+from .PID_optimizers import PID, SparsePID
 from .types import AlternatingType
 from .unconstrained_optimizer import UnconstrainedOptimizer
