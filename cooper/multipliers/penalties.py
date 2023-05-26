89ea6a304eb8d200ab1f0b07a54cd4faf721db44
juan43ramirez <juan43.ramirez@gmail.com>
2023-05-26 11:41:41 -0400

    IndexedPenaltyCoefficient

--- cooper/multipliers/penalties.py
+++ cooper/multipliers/penalties.py
@@ -5,33 +5,74 @@ class PenaltyCoefficient:
     """Constant (non-trainable) coefficient class used for penalized formulations.
 
     Args:
-        init: Value of the penalty.
+        init: Value of the penalty coefficient.
     """
 
     def __init__(self, init: torch.Tensor):
         if init.requires_grad:
-            raise ValueError("PenaltyCoefficient should not be trainable.")
-        self.weight = init
-        self.device = init.device
+            raise ValueError("PenaltyCoefficient should not require gradients.")
+        self._value = init.clone()
 
-    def update_value_(self, value: torch.Tensor):
+    @property
+    def value(self):
+        """Return the current value of the penalty coefficient."""
+        return self._value
+
+    @value.setter
+    def value(self, value: torch.Tensor):
         """Update the value of the penalty."""
-        self.weight.data = value
+        if value.requires_grad:
+            raise ValueError("New value of PenaltyCoefficient should not require gradients.")
+        self._value = value.clone()
+
+    def to(self, device: torch.device = None, dtype: torch.dtype = None):
+        """Move the penalty to a new device and/or change its dtype."""
+        self.value = self.value.to(device=device, dtype=dtype)
 
     def __call__(self):
-        """Return the current value of the multiplier."""
-        return torch.clone(self.weight)
-
-    def parameters(self):
-        """Return an empty iterator for consistency with multipliers which are
-        :py:class:`~torch.nn.Module`."""
-        return iter(())
+        """Return the current value of the penalty coefficient."""
+        return self.value
 
     def state_dict(self):
-        return {"weight": self.weight}
+        return {"value": self._value}
 
     def load_state_dict(self, state_dict):
-        self.weight = state_dict["weight"]
+        self._value = state_dict["value"]
 
     def __repr__(self):
-        return f"PenaltyCoefficient({self.weight})"
+        if self.value.numel() <= 10:
+            return f"PenaltyCoefficient({self.value})"
+        else:
+            return f"PenaltyCoefficient(shape={self.value.shape})"
+
+
+class IndexedPenaltyCoefficient(PenaltyCoefficient):
+    """
+    Constant (non-trainable) coefficient class used for penalized formulations.
+    Can be indexed by a tensor of indices when some constraints are not penalized.
+
+    Args:
+        init: Value of the penalty.
+    """
+
+    def __init__(self, init: torch.Tensor):
+        super(IndexedPenaltyCoefficient, self).__init__(init=init)
+
+    def __call__(self, indices: torch.Tensor):
+        """Return the current value of the penalty coefficient at the provided indices."""
+
+        if indices.dtype != torch.long:
+            # Not allowing for boolean "indices", which are treated as indices by
+            # torch.nn.functional.embedding and *not* as masks.
+            raise ValueError("Indices must be of type torch.long.")
+
+        coefficient_values = torch.nn.functional.embedding(indices, self._value, sparse=False)
+
+        # Flatten coefficient values to 1D since Embedding works with 2D tensors.
+        return torch.flatten(coefficient_values)
+
+    def __repr__(self):
+        if self.value.numel() <= 10:
+            return f"IndexedPenaltyCoefficient({self.value})"
+        else:
+            return f"IndexedPenaltyCoefficient(shape={self.value.shape})"
