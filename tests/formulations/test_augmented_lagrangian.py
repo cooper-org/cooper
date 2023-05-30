040a02f4d34ea0a0f84dbdb9cea80fbbb9b3c93f
juan43ramirez <juan43.ramirez@gmail.com>
2023-05-30 15:22:50 -0400

    Test saving and loading of AL

--- tests/formulations/test_augmented_lagrangian.py
+++ tests/formulations/test_augmented_lagrangian.py
@@ -1,3 +1,6 @@
+import os
+import tempfile
+
 import cooper_test_utils
 import pytest
 import testing_utils
@@ -109,11 +112,7 @@ def test_manual_augmented_lagrangian_dual_primal():
     "alternating_type", [cooper.optim.AlternatingType.PRIMAL_DUAL, cooper.optim.AlternatingType.DUAL_PRIMAL, False]
 )
 def test_convergence_augmented_lagrangian(
-    alternating_type,
-    Toy2dCMP_params_init,
-    Toy2dCMP_problem_properties,
-    use_multiple_primal_optimizers,
-    device,
+    alternating_type, Toy2dCMP_params_init, Toy2dCMP_problem_properties, use_multiple_primal_optimizers, device
 ):
     """Test convergence of Augmented Lagrangian updates on toy 2D problem."""
 
@@ -145,3 +144,76 @@ def test_convergence_augmented_lagrangian(
     for param, exact_solution in zip(params, Toy2dCMP_problem_properties["exact_solution"]):
         # NOTE: this test requires a relaxed tolerance of 1e-4
         assert torch.allclose(param, exact_solution, atol=1e-4)
+
+
+def test_save_and_load_state_dict(Toy2dCMP_params_init, device):
+    params, primal_optimizers = cooper_test_utils.build_params_and_primal_optimizers(
+        use_multiple_primal_optimizers=False, params_init=Toy2dCMP_params_init
+    )
+
+    alternating = cooper.optim.AlternatingType.FALSE
+
+    cmp, cooper_optimizer, const1_penalty_coefficient, const2_penalty_coefficient = setup_augmented_lagrangian_objects(
+        primal_optimizers, alternating, device
+    )
+
+    roll_kwargs = {"compute_cmp_state_fn": lambda: cmp.compute_cmp_state(params)}
+
+    for _ in range(10):
+        cooper_optimizer.roll(**roll_kwargs)
+        # Multiply the penalty coefficients by 1.01
+        const1_penalty_coefficient.value = const1_penalty_coefficient() * 1.01
+        const2_penalty_coefficient.value = const2_penalty_coefficient() * 1.01
+
+    # Generate checkpoints after 10 steps of training
+    penalty1_after10 = const1_penalty_coefficient().clone().detach()
+    penalty2_after10 = const2_penalty_coefficient().clone().detach()
+    multiplier1_after10 = cmp.constraint_groups[0].multiplier().clone().detach()
+    multiplier2_after10 = cmp.constraint_groups[1].multiplier().clone().detach()
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        torch.save(cmp.constraint_groups[0].state_dict(), os.path.join(tmpdirname, "cg0.pt"))
+        torch.save(cmp.constraint_groups[1].state_dict(), os.path.join(tmpdirname, "cg1.pt"))
+
+        cg0_state_dict = torch.load(os.path.join(tmpdirname, "cg0.pt"))
+        cg1_state_dict = torch.load(os.path.join(tmpdirname, "cg1.pt"))
+
+    # Train for another 10 steps
+    for _ in range(10):
+        cooper_optimizer.roll(**roll_kwargs)
+        const1_penalty_coefficient.value = const1_penalty_coefficient() * 1.01
+        const2_penalty_coefficient.value = const2_penalty_coefficient() * 1.01
+
+    # Reload from checkpoint
+    new_const1_penalty_coefficient = cooper.multipliers.PenaltyCoefficient(torch.tensor(1.0, device=device))
+    new_const2_penalty_coefficient = cooper.multipliers.PenaltyCoefficient(torch.tensor(1.0, device=device))
+    new_cmp = cooper_test_utils.Toy2dCMP(
+        use_ineq_constraints=True,
+        formulation_type=cooper.FormulationType.AUGMENTED_LAGRANGIAN,
+        penalty_coefficients=[new_const1_penalty_coefficient, new_const2_penalty_coefficient],
+        device=device,
+    )
+    new_cmp.constraint_groups[0].load_state_dict(cg0_state_dict)
+    new_cmp.constraint_groups[1].load_state_dict(cg1_state_dict)
+
+    # The loaded penalty coefficients come from 10 steps of training, so they should be
+    # different from the current ones
+    new_penalty1_value = new_const1_penalty_coefficient().clone().detach()
+    new_penalty2_value = new_const2_penalty_coefficient().clone().detach()
+    assert not torch.allclose(new_penalty1_value, const1_penalty_coefficient())
+    assert not torch.allclose(new_penalty2_value, const2_penalty_coefficient())
+
+    # They should, however, be the same as the ones recorded before the checkpoint
+    assert torch.allclose(new_penalty1_value, penalty1_after10)
+    assert torch.allclose(new_penalty2_value, penalty2_after10)
+
+    # Similar story for the multipliers
+    new_multiplier1_value = new_cmp.constraint_groups[0].multiplier().clone().detach()
+    new_multiplier2_value = new_cmp.constraint_groups[1].multiplier().clone().detach()
+
+    if new_multiplier1_value != 0:
+        assert not torch.allclose(new_multiplier1_value, cmp.constraint_groups[0].multiplier())
+    if new_multiplier2_value != 0:
+        assert not torch.allclose(new_multiplier2_value, cmp.constraint_groups[1].multiplier())
+    assert torch.allclose(new_multiplier1_value, multiplier1_after10)
+    assert torch.allclose(new_multiplier2_value, multiplier2_after10)
