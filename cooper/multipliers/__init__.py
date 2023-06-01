4e13f2a466203c816f770d63ef7c3b8ae59686da
Jose Gallego-Posada <8620503+gallego-posada@users.noreply.github.com>
2023-06-01 19:45:49 -0400

    Rename module to `penalty_coefficients`

--- cooper/multipliers/__init__.py
+++ cooper/multipliers/__init__.py
@@ -2,6 +2,6 @@ from typing import Union
 
 from .builders import build_explicit_multiplier
 from .multipliers import DenseMultiplier, ExplicitMultiplier, ImplicitMultiplier, IndexedMultiplier
-from .penalties import PenaltyCoefficient
+from .penalty_coefficients import PenaltyCoefficient, IndexedPenaltyCoefficient
 
 MULTIPLIER_TYPE = Union[DenseMultiplier, IndexedMultiplier, ImplicitMultiplier]
