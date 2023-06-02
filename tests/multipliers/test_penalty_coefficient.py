422cf7c8cbbc4dcc360120e00a7fb69550c926ed
Jose Gallego-Posada <8620503+gallego-posada@users.noreply.github.com>
2023-06-01 20:08:36 -0400

    Fix correct class in coefficient test

--- tests/multipliers/test_penalty_coefficient.py
+++ tests/multipliers/test_penalty_coefficient.py
@@ -6,5 +6,5 @@ from cooper import multipliers
 
 
 def test_penalty_coefficient_init_and_forward(_init_tensor):
-    penalty_coefficient = multipliers.PenaltyCoefficient(_init_tensor)
+    penalty_coefficient = multipliers.DensePenaltyCoefficient(_init_tensor)
     assert torch.allclose(penalty_coefficient(), _init_tensor)
