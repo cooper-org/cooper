eb5a80355af08df8ced725ec98be9c8cfc46ad16
Jose Gallego-Posada <8620503+gallego-posada@users.noreply.github.com>
2023-06-01 21:43:58 -0400

    Update multiplier __init__

--- cooper/multipliers/__init__.py
+++ cooper/multipliers/__init__.py
@@ -1,7 +1,43 @@
-from typing import Union
+from enum import Enum
+from typing import Optional, Union
+
+import torch
+
+from cooper.constraints.constraint_state import ConstraintState
 
 from .builders import build_explicit_multiplier
-from .multipliers import DenseMultiplier, ExplicitMultiplier, ImplicitMultiplier, IndexedMultiplier
-from .penalty_coefficients import PenaltyCoefficient, IndexedPenaltyCoefficient
+from .multipliers import DenseMultiplier, ExplicitMultiplier, ImplicitMultiplier, IndexedMultiplier, Multiplier
+from .penalty_coefficients import DensePenaltyCoefficient, IndexedPenaltyCoefficient, PenaltyCoefficient
 
-MULTIPLIER_TYPE = Union[DenseMultiplier, IndexedMultiplier, ImplicitMultiplier]
+ConstraintFactor = Union[Multiplier, PenaltyCoefficient]
+
+
+def evaluate_constraint_factor(module: ConstraintFactor, constraint_state: ConstraintState) -> torch.Tensor:
+    """Evaluate the Lagrange multiplier or penalty coefficient associated with a
+    constraint group.
+
+    Args:
+        module: Multiplier or penalty coefficient.
+        constraint_state: The current state of the constraint.
+    """
+    value: torch.Tensor
+    if constraint_state.constraint_features is None:
+        value = module()
+    else:
+        value = module(constraint_state.constraint_features)
+
+    if len(value.shape) == 0:
+        value.unsqueeze_(0)
+
+    return value
+
+
+class MultiplierType(Enum):
+    DENSE = DenseMultiplier
+    INDEXED = IndexedMultiplier
+    IMPLICIT = ImplicitMultiplier
+
+
+class PenaltyCoefficientType(Enum):
+    DENSE = DensePenaltyCoefficient
+    INDEXED = IndexedPenaltyCoefficient
