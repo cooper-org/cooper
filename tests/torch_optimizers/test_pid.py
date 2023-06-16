b5059c3cec2e845267e041e347a2a59bf6398f99
juan43ramirez <juan43.ramirez@gmail.com>
2023-06-16 08:39:14 -0500

    test sparse PID

--- tests/torch_optimizers/test_pid.py
+++ tests/torch_optimizers/test_pid.py
@@ -6,61 +6,62 @@ import cooper
 from cooper.multipliers import IndexedMultiplier
 from cooper.optim import PID, SparsePID
 
-# @pytest.mark.parametrize(["proportional", "integral", "derivative"], [(0, 1, 0), (1, 1, 0), (0, 1, 1), (1, 1, 1)])
-# def test_manual_pid(proportional, integral, derivative):
-#     param = torch.tensor([1.0], requires_grad=True)
 
-#     loss_fn = lambda param: param**2 / 2
+@pytest.mark.parametrize(["proportional", "integral", "derivative"], [(0, 1, 0), (1, 1, 0), (0, 1, 1), (1, 1, 1)])
+def test_manual_pid(proportional, integral, derivative):
+    param = torch.tensor([1.0], requires_grad=True)
 
-#     lr = 0.1
+    loss_fn = lambda param: param**2 / 2
 
-#     def PID_direction(grad, change, curvature):
-#         return integral * grad + proportional * change + derivative * curvature
+    lr = 0.1
 
-#     optimizer = PID([param], lr=lr, proportional=proportional, integral=integral, derivative=derivative)
+    def PID_direction(grad, change, curvature):
+        return integral * grad + proportional * change + derivative * curvature
 
-#     def do_step():
-#         optimizer.zero_grad()
-#         loss = loss_fn(param)
-#         loss.backward()
-#         optimizer.step()
+    optimizer = PID([param], lr=lr, proportional=proportional, integral=integral, derivative=derivative)
 
-#     # -------------------------------------------- First step of optimization
+    def do_step():
+        optimizer.zero_grad()
+        loss = loss_fn(param)
+        loss.backward()
+        optimizer.step()
 
-#     # Manual first step of optimization. The gradient is simply the current value of p.
-#     p0 = param.clone().detach()
-#     p1 = p0 - lr * PID_direction(p0, 0.0, 0.0)
+    # -------------------------------------------- First step of optimization
 
-#     do_step()
+    # Manual first step of optimization. The gradient is simply the current value of p.
+    p0 = param.clone().detach()
+    p1 = p0 - lr * PID_direction(p0, 0.0, 0.0)
 
-#     assert torch.allclose(param, p1)
+    do_step()
 
-#     # Check the state of the optimizer. Should contain the first gradient in
-#     # `previous_direction` and also in `previous_change`.
-#     assert torch.allclose(optimizer.state[param]["previous_direction"], p0)
-#     assert "previous_change" not in optimizer.state[param]
+    assert torch.allclose(param, p1)
 
-#     # -------------------------------------------- Second step of optimization
-#     p2 = p1 - lr * PID_direction(p1, p1 - p0, 0.0)
+    # Check the state of the optimizer. Should contain the first gradient in
+    # `previous_direction` and also in `previous_change`.
+    assert torch.allclose(optimizer.state[param]["previous_direction"], p0)
+    assert "previous_change" not in optimizer.state[param]
 
-#     do_step()
+    # -------------------------------------------- Second step of optimization
+    p2 = p1 - lr * PID_direction(p1, p1 - p0, 0.0)
 
-#     assert torch.allclose(param, p2)
+    do_step()
 
-#     # The state contain p1 in `previous_direction` and p1 - p0 in `previous_change`.
-#     assert torch.allclose(optimizer.state[param]["previous_direction"], p1)
-#     assert torch.allclose(optimizer.state[param]["previous_change"], p1 - p0)
+    assert torch.allclose(param, p2)
 
-#     # -------------------------------------------- Third step of optimization
-#     p3 = p2 - lr * PID_direction(p2, p2 - p1, p2 - 2 * p1 + p0)
+    # The state contain p1 in `previous_direction` and p1 - p0 in `previous_change`.
+    assert torch.allclose(optimizer.state[param]["previous_direction"], p1)
+    assert torch.allclose(optimizer.state[param]["previous_change"], p1 - p0)
 
-#     do_step()
+    # -------------------------------------------- Third step of optimization
+    p3 = p2 - lr * PID_direction(p2, p2 - p1, p2 - 2 * p1 + p0)
 
-#     assert torch.allclose(param, p3)
+    do_step()
 
-#     # The state contain p2 in `previous_direction` and p2 - p1 in `previous_change`.
-#     assert torch.allclose(optimizer.state[param]["previous_direction"], p2)
-#     assert torch.allclose(optimizer.state[param]["previous_change"], p2 - p1)
+    assert torch.allclose(param, p3)
+
+    # The state contain p2 in `previous_direction` and p2 - p1 in `previous_change`.
+    assert torch.allclose(optimizer.state[param]["previous_direction"], p2)
+    assert torch.allclose(optimizer.state[param]["previous_change"], p2 - p1)
 
 
 @pytest.mark.parametrize(["proportional", "integral", "derivative"], [(0, 1, 0), (1, 1, 0), (0, 1, 1), (1, 1, 1)])
@@ -96,8 +97,8 @@ def test_manual_sparse_pid(proportional, integral, derivative):
     # Manually calculate the state of the optimizer
     previous_direction = torch.zeros_like(p0)
     previous_direction[indices] = p0.clone()[indices]
+    # For the first step, the previous_change is 0 for all indices
     previous_change = torch.zeros_like(p0)
-    previous_change[indices] = p0.clone()[indices]
 
     optimizer.zero_grad()
     loss = loss_fn(param, indices)
@@ -120,13 +121,19 @@ def test_manual_sparse_pid(proportional, integral, derivative):
 
     # Manual second step
     p2 = p1.clone()
-    p2[indices] = p1[indices] - lr * PID_direction(p1, p1 - previous_direction, 0.0)[indices]
+    change = p1 - previous_direction
+
+    unseen_indices = torch.ones_like(all_indices, dtype=torch.bool)
+    unseen_indices[old_indices] = False
+    change[unseen_indices] = 0.0
+
+    p2[indices] -= lr * PID_direction(p1, change, 0.0)[indices]
 
     previous_direction[indices] = p1.clone()[indices]
 
     # We modify previous_change only for the indices that were updated.
-    # For indices that were only observed now, the previous_change is p1 - 0
-    previous_change[indices] = p1[indices] - 0.0
+    # For indices that were only observed now, the previous_change is 0
+    previous_change[indices] = torch.zeros_like(p1[indices])
 
     # For indices that were observed before *and* now, the previous_change is p1 - p0.
     # Note that the following line overwites the previous line for the indices that
@@ -146,43 +153,45 @@ def test_manual_sparse_pid(proportional, integral, derivative):
     assert torch.allclose(state["previous_direction"].flatten(), previous_direction)
     assert torch.allclose(state["previous_change"].flatten(), previous_change)
 
+    # -------------------------------------------- Third step of optimization
 
-# @pytest.mark.parametrize(["proportional", "integral", "derivative"], [(0, 1, 0), (1, 1, 0), (0, 1, 1), (1, 1, 1)])
-# def test_pid_convergence(
-#     proportional,
-#     integral,
-#     derivative,
-#     Toy2dCMP_problem_properties,
-#     Toy2dCMP_params_init,
-#     use_multiple_primal_optimizers,
-#     device,
-# ):
-#     """Test convergence of PID updates on toy 2D problem. The PID updates are only
-#     applied to the dual variables."""
 
-#     use_ineq_constraints = Toy2dCMP_problem_properties["use_ineq_constraints"]
-#     if not use_ineq_constraints:
-#         pytest.skip("Test requires a problem with constraints.")
+@pytest.mark.parametrize(["proportional", "integral", "derivative"], [(0, 1, 0), (1, 1, 0), (0, 1, 1), (1, 1, 1)])
+def test_pid_convergence(
+    proportional,
+    integral,
+    derivative,
+    Toy2dCMP_problem_properties,
+    Toy2dCMP_params_init,
+    use_multiple_primal_optimizers,
+    device,
+):
+    """Test convergence of PID updates on toy 2D problem. The PID updates are only
+    applied to the dual variables."""
 
-#     cmp = cooper_test_utils.Toy2dCMP(use_ineq_constraints=use_ineq_constraints, device=device)
+    use_ineq_constraints = Toy2dCMP_problem_properties["use_ineq_constraints"]
+    if not use_ineq_constraints:
+        pytest.skip("Test requires a problem with constraints.")
 
-#     params, primal_optimizers = cooper_test_utils.build_params_and_primal_optimizers(
-#         use_multiple_primal_optimizers=use_multiple_primal_optimizers, params_init=Toy2dCMP_params_init
-#     )
+    cmp = cooper_test_utils.Toy2dCMP(use_ineq_constraints=use_ineq_constraints, device=device)
 
-#     dual_params = [{"params": constraint.multiplier.parameters()} for constraint in cmp.constraint_groups]
-#     dual_optimizer = PID(
-#         dual_params, lr=0.01, proportional=proportional, integral=integral, derivative=derivative, maximize=True
-#     )
+    params, primal_optimizers = cooper_test_utils.build_params_and_primal_optimizers(
+        use_multiple_primal_optimizers=use_multiple_primal_optimizers, params_init=Toy2dCMP_params_init
+    )
 
-#     cooper_optimizer = cooper.optim.SimultaneousOptimizer(
-#         primal_optimizers, dual_optimizer, constraint_groups=cmp.constraint_groups
-#     )
+    dual_params = [{"params": constraint.multiplier.parameters()} for constraint in cmp.constraint_groups]
+    dual_optimizer = PID(
+        dual_params, lr=0.01, proportional=proportional, integral=integral, derivative=derivative, maximize=True
+    )
 
-#     compute_cmp_state_fn = lambda: cmp.compute_cmp_state(params)
+    cooper_optimizer = cooper.optim.SimultaneousOptimizer(
+        primal_optimizers, dual_optimizer, constraint_groups=cmp.constraint_groups
+    )
 
-#     for step_id in range(1500):
-#         cooper_optimizer.roll(compute_cmp_state_fn)
+    compute_cmp_state_fn = lambda: cmp.compute_cmp_state(params)
 
-#     for param, exact_solution in zip(params, Toy2dCMP_problem_properties["exact_solution"]):
-#         assert torch.allclose(param, exact_solution)
+    for step_id in range(1500):
+        cooper_optimizer.roll(compute_cmp_state_fn)
+
+    for param, exact_solution in zip(params, Toy2dCMP_problem_properties["exact_solution"]):
+        assert torch.allclose(param, exact_solution)
