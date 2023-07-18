afb45bb6b09bb4c495ae448b0049f7fcf086298f
juan43ramirez <juan43.ramirez@gmail.com>
2023-07-18 11:38:39 -0500

    Update PID documentation

--- cooper/optim/PID_optimizers.py
+++ cooper/optim/PID_optimizers.py
@@ -9,6 +9,7 @@ common to min-max optimization problems.
 """
 
 import warnings
+from typing import Optional
 
 import torch
 
@@ -18,31 +19,40 @@ import torch
 
 class PID(torch.optim.Optimizer):
     r"""
-    TODO(juan43ramirez): complete docstring
+    Consider a variable x_t, and its gradient g_t = \nabla f(x_t) at time t. This
+    optimizer applies PID control to x_t in order to drive g_t to zero and thus find a
+    stationary point of f.
 
-    PID is x_t = x_0 + ...
-    This implementation is recursive: x_t = x_{t-1} + ...
+    The unrolled PID update is:
+        x_{t+1} = x_0 - lr * (Ki * \sum_{i=0}^t g_i + Kp * g_t + Kd * (g_t - g_{t-1}))
 
-    The default for p=0, i=1, d=0 corresponds to SGD with learning rate lr
-    If p!=0, i!=0, d=0, then the optimizer corresponds to an optimistic gradient descent
+    Where Ki is the integral gain, Kp is the proportional gain, and Kd is the derivative
+    gain.
 
-    Mention memory  and computational complexity wrt SGD
+    This implementation is *recursive*:
+        x_{t+1} = x_t - lr * (Ki * g_t + Kp * (g_t - g_{t-1}) + Kd * (g_t - 2 * g_{t-1} + g_{t-2}))
+
+    Note that setting Ki=1, Kp=0, Kd=0 corresponds to SGD with learning rate lr
+    Setting Ki=1, Kp=1, Kd=0 corresponds to the optimistic gradient method
 
     Arguments:
-        params (iterable): iterable of parameters to optimize or dicts defining
-        ...
+        params: iterable of parameters to optimize or dicts defining parameter groups
+        lr: learning rate
+        weight_decay: weight decay (L2 penalty)
+        Kp: proportional gain
+        Ki: integral gain
+        Kd: derivative gain
+        maximize: whether to maximize or minimize the loss
     """
-
-    # TODO: Kp -> Kp
 
     def __init__(
         self,
         params,
         lr: float,
-        weight_decay: float = 0.0,
-        Kp: float = 0.0,
+        weight_decay: Optional[float] = 0.0,
+        Kp: Optional[float] = 0.0,
         Ki: float = 1.0,
-        Kd: float = 0.0,
+        Kd: Optional[float] = 0.0,
         maximize: bool = False,
     ):
         if not 0.0 <= lr:
@@ -59,19 +69,12 @@ class PID(torch.optim.Optimizer):
         if all([Kp == 0.0, Ki == 0.0, Kd == 0.0]):
             warnings.warn("Invalid PID coefficients: all are zero")
 
-        defaults = dict(
-            lr=lr,
-            weight_decay=weight_decay,
-            Kp=Kp,
-            Ki=Ki,
-            Kd=Kd,
-            maximize=maximize,
-        )
+        defaults = dict(lr=lr, weight_decay=weight_decay, Kp=Kp, Ki=Ki, Kd=Kd, maximize=maximize)
 
         super().__init__(params, defaults)
 
     @torch.no_grad()
-    def step(self, closure=None):
+    def step(self, closure=None) -> Optional[float]:
         """Performs a single optimization step.
 
         Args:
@@ -85,9 +88,7 @@ class PID(torch.optim.Optimizer):
         for group in self.param_groups:
             lr = group["lr"]
             weight_decay = group["weight_decay"]
-            Kp = group["Kp"]
-            Ki = group["Ki"]
-            Kd = group["Kd"]
+            Kp, Ki, Kd = group["Kp"], group["Ki"], group["Kd"]
             maximize = group["maximize"]
 
             for p in group["params"]:
@@ -101,7 +102,13 @@ class PID(torch.optim.Optimizer):
         return loss
 
 
-def _estimate_change_and_curvature(grad, state):
+def _estimate_change_and_curvature(grad: torch.Tensor, state: dict) -> tuple[torch.Tensor, torch.Tensor]:
+    """
+    Calculate the change in direction and curvature of the gradient.
+        change_t = grad_t - grad_{t-1} = `grad` - `state["previous_direction"]`
+        curvature_t = change_t - change_{t-1} = change_t - `state["previous_change"]`
+    """
+
     if len(state) == 0:
         # At this stage, there is not enough information to compute the
         # change in direction for the P term nor the curvature for the D
@@ -121,11 +128,29 @@ def _estimate_change_and_curvature(grad, state):
     return change, curvature
 
 
-def _pid(param, state, lr, weight_decay, Kp, Ki, Kd, maximize):
-    if param.grad.is_sparse:
-        raise RuntimeError("PID optimizer does not support sparse gradients. Consider SparsePID instead.")
+def _pid(
+    param: torch.Tensor, state: dict, lr: float, weight_decay: float, Kp: float, Ki: float, Kd: float, maximize: bool
+):
+    """
+    Applies a PID step update to `param`
+
+    The general form of the update (for minimization, without weight decay) is:
+        change_t = grad_t - grad_{t-1}
+        curvature_t = change_t - change_{t-1} = grad_t - 2 * grad_{t-1} + grad_{t-2}
+        x_{t+1} = x_t - lr * (Ki * grad_t + Kp * change_t + Kd * curvature_t)
+
+    Note that there is not enough information to compute change_0, curvature_0, nor
+    curvature_1. Therefore, we define the following convention:
+        change_0 = curvature_0 = curvature_1 = 0
+
+    This means that the first update corresponds to SGD with learning rate lr * Ki:
+        x_1 = x_0 - lr * Ki * grad_0
+    And the second update corresponds to the optimistic gradient method:
+        x_2 = x_1 - lr * (Ki * grad_1 + Kp * (grad_1 - grad_0))
+    """
 
     grad = param.grad
+    assert not grad.is_sparse
 
     change, curvature = _estimate_change_and_curvature(grad, state)
 
@@ -136,7 +161,7 @@ def _pid(param, state, lr, weight_decay, Kp, Ki, Kd, maximize):
     if Kd != 0:
         d_p.add_(curvature, alpha=Kd)
 
-    # Weight decay is applied after estimating the change and curvsture, similar to
+    # Weight decay is applied after estimating the change and curvature, similar to
     # AdamW. See https://arxiv.org/abs/1711.05101 for details.
     if weight_decay != 0:
         d_p.add_(param, alpha=weight_decay)
@@ -147,6 +172,8 @@ def _pid(param, state, lr, weight_decay, Kp, Ki, Kd, maximize):
     param.add_(d_p, alpha=-lr)
 
     if len(state) == 0:
+        # Lazily initialize the state after the first update.
+        #
         # Only the I term was used for the first update. For the next step,
         # the current update direction will be used to compute the P term.
         # We do not initialize `previous_change` as a convention to indicate
@@ -157,12 +184,15 @@ def _pid(param, state, lr, weight_decay, Kp, Ki, Kd, maximize):
         state["previous_change"] = change.clone().detach()
 
 
-def _sparse_pid(params, state, lr, weight_decay, Kp, Ki, Kd, maximize):
+def _sparse_pid(
+    param: torch.Tensor, state: dict, lr: float, weight_decay: float, Kp: float, Ki: float, Kd: float, maximize: bool
+):
     r"""
-    Supports sparse gradient updates. Inspired by SparseAdam from PyTorch.
+    Analogous to _pid but with support for sparse gradients. Inspired by SparseAdam.
     https://github.com/pytorch/pytorch/blob/release/2.0/torch/optim/_functional.py
     """
-    grad = params.grad
+    grad = param.grad
+    assert grad.is_sparse
 
     grad = grad.coalesce()  # the update is non-linear so indices must be unique
     grad_indices = grad._indices()
@@ -173,6 +203,11 @@ def _sparse_pid(params, state, lr, weight_decay, Kp, Ki, Kd, maximize):
     size = grad.size()
 
     def make_sparse(values):
+        """
+        Function to convert a dense tensor of values to a sparse representation with the
+        same indices as `grad`.
+        """
+
         constructor = grad.new
         if grad_indices.dim() == 0 or values.dim() == 0:
             return constructor().resize_as_(grad)
@@ -181,9 +216,9 @@ def _sparse_pid(params, state, lr, weight_decay, Kp, Ki, Kd, maximize):
     if len(state) == 0:
         # NOTE: considering a *dense* state. Note that IndexedMultipliers are
         # stored in a dense representation as well.
-        state["steps"] = torch.zeros_like(params, dtype=torch.int)
-        state["previous_direction"] = torch.zeros_like(params)
-        state["previous_change"] = torch.zeros_like(params)
+        state["steps"] = torch.zeros_like(param, dtype=torch.int)
+        state["previous_direction"] = torch.zeros_like(param)
+        state["previous_change"] = torch.zeros_like(param)
 
     step_counter = state["steps"].sparse_mask(grad)
     previous_direction = state["previous_direction"].sparse_mask(grad)
@@ -207,13 +242,13 @@ def _sparse_pid(params, state, lr, weight_decay, Kp, Ki, Kd, maximize):
     # Weight decay is applied after estimating the change and curvsture, similar to
     # AdamW. See https://arxiv.org/abs/1711.05101 for details.
     if weight_decay != 0:
-        p_values = torch.index_select(params, 0, grad_indices[0])
+        p_values = torch.index_select(param, 0, grad_indices[0])
         d_p_values.add_(p_values, alpha=weight_decay)
 
     if maximize:
         d_p_values.mul_(-1)
 
-    params.add_(make_sparse(d_p_values), alpha=-lr)
+    param.add_(make_sparse(d_p_values), alpha=-lr)
 
     # Update the step counter for observed parameters.
     state["steps"].add_(make_sparse(torch.ones_like(grad_values, dtype=torch.int)))
