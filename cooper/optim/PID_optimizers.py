d773479947160fd090f87ab49fed28edadc0bb88
juan43ramirez <juan43.ramirez@gmail.com>
2023-08-20 17:35:04 -0400

    Tested PID_EMA

--- cooper/optim/PID_optimizers.py
+++ cooper/optim/PID_optimizers.py
@@ -37,7 +37,7 @@ class PID(torch.optim.Optimizer):
     Note that setting Ki=1, Kp=0, Kd=0 corresponds to SGD with learning rate lr
     Setting Ki=1, Kp=1, Kd=0 corresponds to the optimistic gradient method.
 
-    # TODO(juan43ramirez): document the momentum term
+    # TODO(juan43ramirez): document the noise filtering applied to the (D) term.
 
     Arguments:
         params: iterable of parameters to optimize or dicts defining parameter groups
@@ -57,7 +57,7 @@ class PID(torch.optim.Optimizer):
         Kp: Optional[float] = 0.0,
         Ki: float = 1.0,
         Kd: Optional[float] = 0.0,
-        momentum: Optional[float] = 0.0,
+        ema_beta: Optional[float] = 0.0,
         maximize: bool = False,
     ):
         if not 0.0 <= lr:
@@ -65,18 +65,25 @@ class PID(torch.optim.Optimizer):
         if weight_decay < 0.0:
             raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
 
+        # NOTE: allowing for a negative Kp coefficient to allow for the use of PID+EMA
+        # as a generalization of Polyak and Nesterov momentum. For details, see
+        # http://www.argmin.net/2018/04/19/pid/
         if Kp < 0.0:
-            raise ValueError("Invalid Kp coefficient: {}".format(Kp))
+            warnings.warn("Using a negative Kp coefficient: {}".format(Kp))
+            # raise ValueError("Invalid Kp coefficient: {}".format(Kp))
         if Ki < 0.0:
             raise ValueError("Invalid Ki coefficient: {}".format(Ki))
         if Kd < 0.0:
             raise ValueError("Invalid Kd coefficient: {}".format(Kd))
         if all([Kp == 0.0, Ki == 0.0, Kd == 0.0]):
             warnings.warn("Invalid PID coefficients: all are zero")
-        if momentum < 0.0:
-            raise ValueError("Invalid momentum value: {}".format(momentum))
+        if ema_beta < 0.0 or ema_beta >= 1.0:
+            raise ValueError("Invalid EMA coefficient: {}".format(ema_beta))
 
-        defaults = dict(lr=lr, weight_decay=weight_decay, Kp=Kp, Ki=Ki, Kd=Kd, momentum=momentum, maximize=maximize)
+        if ema_beta != 0.0 and Kd == 0.0:
+            warnings.warn("EMA coefficient is non-zero but Kd is zero")
+
+        defaults = dict(lr=lr, weight_decay=weight_decay, Kp=Kp, Ki=Ki, Kd=Kd, ema_beta=ema_beta, maximize=maximize)
 
         super().__init__(params, defaults)
 
@@ -107,17 +114,18 @@ class PID(torch.optim.Optimizer):
                     Kp=group["Kp"],
                     Ki=group["Ki"],
                     Kd=group["Kd"],
-                    momentum=group["momentum"],
+                    ema_beta=group["ema_beta"],
                     maximize=group["maximize"],
                 )
 
         return loss
 
 
-def _estimate_deltas_and_curvature(
-    param, grad: torch.Tensor, state: dict
+def _estimate_delta_and_curvature(
+    grad: torch.Tensor, state: dict, ema_beta: float
 ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     """
+    TODO: update docstring
     Calculate the gradient delta and curvature.
         delta_t = grad_t - grad_{t-1} = `grad` - `state["previous_grad"]`
         curvature_t = delta_t - delta_{t-1} = delta_t - `state["previous_delta"]`
@@ -127,22 +135,23 @@ def _estimate_deltas_and_curvature(
         # At this stage, there is not enough information to compute the parameter or
         # gradient delta for the (P) term nor the curvature for the (D) term. Therefore,
         # only the (I) term is used for the first update.
-        param_delta = 0
         grad_delta = 0
-        curvature = 0
-    elif "previous_delta" not in state:
+        filtered_delta = 0
+        filtered_curvature = 0
+    elif "delta_ema" not in state:
         assert "previous_grad" in state
         # We use the previous grad to compute the momentum and (P) terms, but there is
         # still not enough information to compute the (D) term.
-        param_delta = param.sub(state["previous_param"])
+        # `filtered_delta` is used to initialize the EMA. It it not used for the updates
         grad_delta = grad.sub(state["previous_grad"])
-        curvature = 0
+        filtered_delta = grad_delta
+        filtered_curvature = 0
     else:
-        param_delta = param.sub(state["previous_param"])
         grad_delta = grad.sub(state["previous_grad"])
-        curvature = grad_delta.sub(state["previous_delta"])
+        filtered_delta = state["delta_ema"].mul(ema_beta).add(grad_delta, alpha=1 - ema_beta)
+        filtered_curvature = filtered_delta.sub(state["delta_ema"])
 
-    return param_delta, grad_delta, curvature
+    return grad_delta, filtered_delta, filtered_curvature
 
 
 def _pid(
@@ -153,7 +162,7 @@ def _pid(
     Kp: float,
     Ki: float,
     Kd: float,
-    momentum: float,
+    ema_beta: float,
     maximize: bool,
 ):
     """
@@ -164,7 +173,7 @@ def _pid(
         curvature_t = grad_t - 2 * grad_{t-1} + grad_{t-2} = delta_t - delta_{t-1}
         x_{t+1} = x_t - lr * (Ki * grad_t + Kp * delta_t + Kd * curvature_t)
 
-    # TODO(juan43ramirez): address the momentum term
+    # TODO(juan43ramirez): address the EMA on the (D) term.
 
     Note that there is not enough information to compute delta_0, curvature_0, nor
     curvature_1. Therefore, we define the following convention:
@@ -179,16 +188,14 @@ def _pid(
     grad = param.grad
     assert not grad.is_sparse, "For sparse gradients, use _sparse_pid instead"
 
-    param_delta, grad_delta, curvature = _estimate_deltas_and_curvature(param, grad, state)
+    grad_delta, filtered_delta, filtered_curvature = _estimate_delta_and_curvature(grad, state, ema_beta)
 
     d_p = grad.mul(Ki)
 
-    if momentum != 0:
-        d_p.add_(param_delta, alpha=momentum)
     if Kp != 0:
         d_p.add_(grad_delta, alpha=Kp)
     if Kd != 0:
-        d_p.add_(curvature, alpha=Kd)
+        d_p.add_(filtered_curvature, alpha=Kd)
 
     # Weight decay is applied after estimating the delta and curvature, similar to
     # AdamW. See https://arxiv.org/abs/1711.05101 for details.
@@ -201,16 +208,16 @@ def _pid(
     if len(state) == 0:
         # Lazily initialize the state after the first update.
         # Only the (I) term was used for the first update. For the next step, the
-        # current parameter will be used to compute the momentum term, and the current
-        # gradient will be used to compute the (P) term.
-        # We do not initialize `previous_delta` as a convention to indicate
-        # that the (D) term should not be used in the following update.
+        # current gradient will be used to compute the (P) term.
+        # We do not initialize `delta_ema` as a convention to indicate that the (D) term
+        # should not be used in the following update.
         state["previous_param"] = param.clone().detach()
         state["previous_grad"] = grad.clone().detach()
     else:
         state["previous_param"] = param.clone().detach()
         state["previous_grad"] = grad.clone().detach()
-        state["previous_delta"] = grad_delta.clone().detach()
+        # The delta EMA is initialized to the first delta.
+        state["delta_ema"] = filtered_delta.clone().detach()
 
     param.add_(d_p, alpha=-lr)
 
@@ -223,7 +230,7 @@ def _sparse_pid(
     Kp: float,
     Ki: float,
     Kd: float,
-    momentum: float,
+    ema_beta: float,
     maximize: bool,
 ):
     r"""
@@ -258,9 +265,8 @@ def _sparse_pid(
         # NOTE: considering a *dense* state. Note that tensors related to IndexedMultipliers
         # are stored in a dense representation as well.
         state["steps"] = torch.zeros_like(param, dtype=torch.int)
-        state["previous_param"] = torch.zeros_like(param)
         state["previous_grad"] = torch.zeros_like(param)
-        state["previous_delta"] = torch.zeros_like(param)
+        state["delta_ema"] = torch.zeros_like(param)
 
     step_counter = state["steps"].sparse_mask(grad)
     # Given the available information from previous updates, the deltas and curvature
@@ -268,18 +274,23 @@ def _sparse_pid(
     is_after_first_update = step_counter._values().ge(1)
     is_after_second_update = step_counter._values().ge(2)
 
-    previous_param = state["previous_param"].sparse_mask(grad)
     previous_grad = state["previous_grad"].sparse_mask(grad)
-    previous_delta = state["previous_delta"].sparse_mask(grad)
+    delta_ema = state["delta_ema"].sparse_mask(grad)
 
-    param_delta_values = observed_param_values.sub(previous_param._values()).mul(is_after_first_update.float())
     delta_values = grad_values.sub(previous_grad._values()).mul(is_after_first_update.float())
-    curvature_values = delta_values.sub(previous_delta._values()).mul(is_after_second_update.float())
+
+    # We initialize the `new_ema_values` to the first unfiltered delta
+    is_first_update = step_counter._values().eq(1)
+    first_delta_ema_values = delta_values.clone().detach().mul(is_first_update.float())
+    # For any step after that, we compute the EMA of the deltas.
+    current_delta_ema_values = delta_ema._values().mul(ema_beta).add(delta_values, alpha=1 - ema_beta)
+    current_delta_ema_values = current_delta_ema_values.mul(is_after_second_update.float())
+
+    current_delta_ema_values = current_delta_ema_values.add(first_delta_ema_values)
+    curvature_values = current_delta_ema_values.sub(delta_ema._values()).mul(is_after_second_update.float())
 
     d_p_values = grad_values.mul(Ki)
 
-    if momentum != 0:
-        d_p_values.add_(param_delta_values, alpha=momentum)
     if Kp != 0:
         d_p_values.add_(delta_values, alpha=Kp)
     if Kd != 0:
@@ -296,12 +307,13 @@ def _sparse_pid(
     # Update the step counter for observed parameters.
     state["steps"].add_(make_sparse(torch.ones_like(grad_values, dtype=torch.int)))
 
-    # Update the previous param, grad, and delta for observed parameters. We always
-    # store `previous_grad` for the next update. `previous_delta` is only used for the
+    # Update the previous grad, and delta for observed parameters. We always store
+    # `previous_grad` for the next update. `
+    #
+    # delta_ema` is only used after the
     # second update, so we store it only for the parameters that have been observed
     # after their first update.
-    state["previous_param"][grad_indices] = observed_param_values.clone().detach()
     state["previous_grad"][grad_indices] = grad_values.clone().detach()
-    state["previous_delta"][grad_indices] = delta_values.mul(is_after_first_update.float()).clone().detach()
+    state["delta_ema"][grad_indices] = current_delta_ema_values.clone().detach()
 
     param.add_(make_sparse(d_p_values), alpha=-lr)
--- tests/torch_optimizers/test_pid.py
+++ tests/torch_optimizers/test_pid.py
@@ -7,21 +7,26 @@ from cooper.multipliers import IndexedMultiplier
 from cooper.optim import PID
 
 # TODO(juan43ramirez): test with multiple parameter groups
-# TODO(juan43ramirez): test PID with momentum
+
+# Configurations correspond to: [I, PI, PD, PID, PID-EMA]
+ALL_HYPER_PARAMS = [(0, 1, 0, 0), (1, 1, 0, 0), (0, 1, 1, 0), (1, 1, 1, 0), (1, 1, 1, 0.9)]
 
 
-@pytest.mark.parametrize(["Kp", "Ki", "Kd"], [(0, 1, 0), (1, 1, 0), (0, 1, 1), (1, 1, 1)])
-def test_manual_pid(Kp, Ki, Kd):
+@pytest.mark.parametrize(["Kp", "Ki", "Kd", "ema_beta"], ALL_HYPER_PARAMS)
+def test_manual_pid(Kp, Ki, Kd, ema_beta):
     param = torch.tensor([1.0], requires_grad=True)
 
     loss_fn = lambda param: param**2 / 2
 
     lr = 0.1
 
+    def update_ema(ema, value):
+        return ema_beta * ema + (1 - ema_beta) * value
+
     def PID_direction(grad, change, curvature):
         return Ki * grad + Kp * change + Kd * curvature
 
-    optimizer = PID([param], lr=lr, Kp=Kp, Ki=Ki, Kd=Kd)
+    optimizer = PID([param], lr=lr, Kp=Kp, Ki=Ki, Kd=Kd, ema_beta=ema_beta)
 
     def do_step():
         optimizer.zero_grad()
@@ -40,35 +45,39 @@ def test_manual_pid(Kp, Ki, Kd):
     assert torch.allclose(param, p1)
 
     # Check the state of the optimizer. Should contain the first gradient in
-    # `previous_grad` and also in `previous_delta`.
+    # `previous_grad`, but not `delta_ema` yet.
     assert torch.allclose(optimizer.state[param]["previous_grad"], p0)
-    assert "previous_delta" not in optimizer.state[param]
+    assert "delta_ema" not in optimizer.state[param]
 
     # -------------------------------------------- Second step of optimization
+    # The delta_ema is initialized to the difference of the first two gradients.
+    # However, it is not used for the second step of optimization.
+    delta_ema = p1 - p0
     p2 = p1 - lr * PID_direction(p1, p1 - p0, 0.0)
 
     do_step()
 
     assert torch.allclose(param, p2)
 
-    # The state contain p1 in `previous_grad` and p1 - p0 in `previous_delta`.
+    # The state contain p1 in `previous_grad` and p1 - p0 in `delta_ema`.
     assert torch.allclose(optimizer.state[param]["previous_grad"], p1)
-    assert torch.allclose(optimizer.state[param]["previous_delta"], p1 - p0)
+    assert torch.allclose(optimizer.state[param]["delta_ema"], delta_ema)
 
     # -------------------------------------------- Third step of optimization
-    p3 = p2 - lr * PID_direction(p2, p2 - p1, p2 - 2 * p1 + p0)
+    new_delta_ema = update_ema(delta_ema, p2 - p1)
+    p3 = p2 - lr * PID_direction(p2, p2 - p1, new_delta_ema - delta_ema)
 
     do_step()
 
     assert torch.allclose(param, p3)
 
-    # The state contain p2 in `previous_grad` and p2 - p1 in `previous_delta`.
+    # The state contain p2 in `previous_grad` and p2 - p1 in `delta_ema`.
     assert torch.allclose(optimizer.state[param]["previous_grad"], p2)
-    assert torch.allclose(optimizer.state[param]["previous_delta"], p2 - p1)
+    assert torch.allclose(optimizer.state[param]["delta_ema"], new_delta_ema)
 
 
-@pytest.mark.parametrize(["Kp", "Ki", "Kd"], [(0, 1, 0), (1, 1, 0), (0, 1, 1), (1, 1, 1)])
-def test_manual_sparse_pid(Kp, Ki, Kd):
+@pytest.mark.parametrize(["Kp", "Ki", "Kd", "ema_beta"], ALL_HYPER_PARAMS)
+def test_manual_sparse_pid(Kp, Ki, Kd, ema_beta):
     if not torch.cuda.is_available():
         pytest.skip("Sparse gradients are only supported on GPU.")
 
@@ -85,87 +94,116 @@ def test_manual_sparse_pid(Kp, Ki, Kd):
     def PID_direction(grad, change, curvature):
         return Ki * grad + Kp * change + Kd * curvature
 
-    optimizer = PID(param.parameters(), lr=lr, Kp=Kp, Ki=Ki, Kd=Kd)
+    optimizer = PID(param.parameters(), lr=lr, Kp=Kp, Ki=Ki, Kd=Kd, ema_beta=ema_beta)
+
+    def do_step(indices):
+        optimizer.zero_grad()
+        loss = loss_fn(param, indices)
+        loss.backward()
+        optimizer.step()
 
     # -------------------------------------------- First step of optimization
-    indices = torch.tensor([0, 1, 4, 7], device=device)
+    first_indices = torch.tensor([0, 1, 2, 4, 7], device=device)
 
     # Manual first step of optimization. The gradient is simply the current value of p.
     p0 = param(all_indices).clone().detach()
     p1 = p0.clone()
-    p1[indices] -= lr * PID_direction(p0, 0.0, 0.0)[indices]  # only update the current indices
+    p1[first_indices] -= lr * PID_direction(p0, 0.0, 0.0)[first_indices]  # only update the current indices
 
     # Manually calculate the state of the optimizer
     previous_grad = torch.zeros_like(p0)
-    previous_grad[indices] = p0.clone()[indices]
-    # For the first step, the previous_delta is 0 for all indices
-    previous_delta = torch.zeros_like(p0)
+    previous_grad[first_indices] = p0.clone()[first_indices]
+    # For the first step, the `delta_ema` is 0 for all indices
+    delta_ema = torch.zeros_like(p0)
 
-    optimizer.zero_grad()
-    loss = loss_fn(param, indices)
-    loss.backward()
-    optimizer.step()
+    do_step(first_indices)
 
     assert torch.allclose(param(all_indices), p1)
 
     # Check the state of the optimizer. For observer indices, should contain the first
-    # gradient in `previous_grad` and also in `previous_delta`. 0 otherwise.
+    # gradient in `previous_grad` and also in `delta_ema`. 0 otherwise.
     state = optimizer.state[list(param.parameters())[0]]
 
     assert torch.allclose(state["previous_grad"].flatten(), previous_grad)
-    assert torch.allclose(state["previous_delta"].flatten(), previous_delta)
+    assert torch.allclose(state["delta_ema"].flatten(), delta_ema)
 
     # -------------------------------------------- Second step of optimization
-    old_indices = indices.clone()
-
-    indices = torch.tensor([0, 3, 5, 7, 9], device=device)
+    second_indices = torch.tensor([0, 3, 5, 7, 9], device=device)
 
     # Manual second step
     p2 = p1.clone()
     change = p1 - previous_grad
 
     unseen_indices = torch.ones_like(all_indices, dtype=torch.bool)
-    unseen_indices[old_indices] = False
+    unseen_indices[first_indices] = False
     change[unseen_indices] = 0.0
 
-    p2[indices] -= lr * PID_direction(p1, change, 0.0)[indices]
+    p2[second_indices] -= lr * PID_direction(p1, change, 0.0)[second_indices]
 
-    previous_grad[indices] = p1.clone()[indices]
+    previous_grad[second_indices] = p1.clone()[second_indices]
 
-    # We modify previous_delta only for the indices that were updated.
-    # For indices that were only observed now, the previous_delta is 0
-    previous_delta[indices] = torch.zeros_like(p1[indices])
+    # For indices that were observed before *and* now, the delta_ema is p1 - p0.
+    # Note that no EMA calculation is being applied yet.
+    twice_observed_indices = [idx for idx in range(10) if idx in first_indices and idx in second_indices]
+    delta_ema[twice_observed_indices] = p1[twice_observed_indices] - p0[twice_observed_indices]
 
-    # For indices that were observed before *and* now, the previous_delta is p1 - p0.
-    # Note that the following line overwites the previous line for the indices that
-    # belong to both old_indices and indices.
-    twice_observed_indices = [idx for idx in range(10) if idx in old_indices and idx in indices]
-    previous_delta[twice_observed_indices] = p1[twice_observed_indices] - p0[twice_observed_indices]
-
-    optimizer.zero_grad()
-    loss = loss_fn(param, indices)
-    loss.backward()
-    optimizer.step()
+    do_step(second_indices)
 
     assert torch.allclose(param(all_indices), p2)
 
     state = optimizer.state[list(param.parameters())[0]]
 
     assert torch.allclose(state["previous_grad"].flatten(), previous_grad)
-    assert torch.allclose(state["previous_delta"].flatten(), previous_delta)
+    assert torch.allclose(state["delta_ema"].flatten(), delta_ema)
 
-    # -------------------------------------------- Third step of optimization
+    #  -------------------------------------------- Third step of optimization
+    third_indices = torch.tensor([0, 2, 4, 7], device=device)
+
+    # Manual third step
+    p3 = p2.clone()
+
+    # Change is simply p2 - previous_grad
+    change = p2 - previous_grad
+
+    # Calculate the delta_ema
+    new_delta_ema = delta_ema.clone()
+    # * For indices observed only once in the past, and also now:
+    #  * If they are part of first_indices, the delta_ema is p2 - p0
+    observed_now_and_first = [idx for idx in range(10) if idx in first_indices and idx in third_indices]
+    new_delta_ema[observed_now_and_first] = p2[observed_now_and_first] - p0[observed_now_and_first]
+    #  * If they are part of second_indices, the delta_ema is p2 - p1
+    observed_now_and_second = [idx for idx in range(10) if idx in second_indices and idx in third_indices]
+    new_delta_ema[observed_now_and_second] = p2[observed_now_and_second] - p1[observed_now_and_second]
+    # * For indices observed twice in the past, delta_ema is:
+    #   ema_beta * delta_ema + (1 - ema_beta) * change
+    observed_thrice = [
+        idx for idx in range(10) if idx in first_indices and idx in second_indices and idx in third_indices
+    ]
+
+    new_delta_ema[observed_thrice] = ema_beta * delta_ema[observed_thrice] + (1 - ema_beta) * change[observed_thrice]
+
+    # Although the new_delta_ema is calculated for indices from `observer_now_and_first`
+    # and `observer_now_and_second`, since the old delta_ema was 0 for those indices,
+    # curvature is not estimated for them.
+    curvature = torch.zeros_like(p2)
+    curvature[observed_thrice] = new_delta_ema[observed_thrice] - delta_ema[observed_thrice]
+
+    p3[third_indices] -= lr * PID_direction(p2, change, curvature)[third_indices]
+
+    do_step(third_indices)
+    state = optimizer.state[list(param.parameters())[0]]
+
+    assert torch.allclose(param(all_indices), p3)
+
+    previous_grad[third_indices] = p2.clone()[third_indices]
+
+    assert torch.allclose(state["previous_grad"].flatten(), previous_grad)
+    assert torch.allclose(state["delta_ema"].flatten(), new_delta_ema)
 
 
-@pytest.mark.parametrize(["Kp", "Ki", "Kd"], [(0, 1, 0), (1, 1, 0), (0, 1, 1), (1, 1, 1)])
+@pytest.mark.parametrize(["Kp", "Ki", "Kd", "ema_beta"], ALL_HYPER_PARAMS)
 def test_pid_convergence(
-    Kp,
-    Ki,
-    Kd,
-    Toy2dCMP_problem_properties,
-    Toy2dCMP_params_init,
-    use_multiple_primal_optimizers,
-    device,
+    Kp, Ki, Kd, ema_beta, Toy2dCMP_problem_properties, Toy2dCMP_params_init, use_multiple_primal_optimizers, device
 ):
     """Test convergence of PID updates on toy 2D problem. The PID updates are only
     applied to the dual variables."""
@@ -181,7 +219,7 @@ def test_pid_convergence(
     )
 
     dual_params = [{"params": constraint.multiplier.parameters()} for constraint in cmp.constraint_groups]
-    dual_optimizer = PID(dual_params, lr=0.01, Kp=Kp, Ki=Ki, Kd=Kd, maximize=True)
+    dual_optimizer = PID(dual_params, lr=0.01, Kp=Kp, Ki=Ki, Kd=Kd, ema_beta=ema_beta, maximize=True)
 
     cooper_optimizer = cooper.optim.SimultaneousOptimizer(
         primal_optimizers, dual_optimizer, constraint_groups=cmp.constraint_groups
