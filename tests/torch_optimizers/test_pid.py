27eeb0d15a99cc24539868a84ace7f96ddecfb7f
Jose Gallego-Posada <8620503+gallego-posada@users.noreply.github.com>
2023-09-16 13:57:52 -0400

    Update dense PID manual test

--- tests/torch_optimizers/test_pid.py
+++ tests/torch_optimizers/test_pid.py
@@ -8,76 +8,104 @@ from cooper.optim import PID
 
 # TODO(juan43ramirez): test with multiple parameter groups
 
-# Configurations correspond to: [I, PI, PD, PID, PID-EMA]
-ALL_HYPER_PARAMS = [(0, 1, 0, 0), (1, 1, 0, 0), (0, 1, 1, 0), (1, 1, 1, 0), (1, 1, 1, 0.9)]
+ALL_HYPER_PARAMS = [
+    (0, 1, 0, 0),  # I controller
+    (1, 1, 0, 0),  # PI controller
+    (0, 1, 1, 0),  # PD controller
+    (1, 1, 1, 0),  # PID controller
+    (1, 1, 1, 0.9),  # PID controller with EMA
+]
 
 
-@pytest.mark.parametrize(["Kp", "Ki", "Kd", "ema_beta"], ALL_HYPER_PARAMS)
-def test_manual_pid(Kp, Ki, Kd, ema_beta):
+@pytest.mark.parametrize(["Kp", "Ki", "Kd", "ema_nu"], ALL_HYPER_PARAMS)
+def test_manual_pid(Kp, Ki, Kd, ema_nu):
     param = torch.tensor([1.0], requires_grad=True)
 
     loss_fn = lambda param: param**2 / 2
 
     lr = 0.1
 
+    def compute_gradient():
+        # For the quadratic loss, the gradient is simply the current value of p.
+        return param.clone().detach()
+
     def update_ema(ema, value):
-        return ema_beta * ema + (1 - ema_beta) * value
+        return ema_nu * ema + (1 - ema_nu) * value
 
-    def PID_direction(grad, change, curvature):
-        return Ki * grad + Kp * change + Kd * curvature
+    def recursive_PID_direction(error, error_change, delta_change):
+        return Kp * error_change + Ki * error + Kd * delta_change
 
-    optimizer = PID([param], lr=lr, Kp=Kp, Ki=Ki, Kd=Kd, ema_beta=ema_beta)
+    optimizer = PID([param], lr=lr, Kp=Kp, Ki=Ki, Kd=Kd, ema_nu=ema_nu, maximize=False)
 
-    def do_step():
+    def do_optimizer_step():
         optimizer.zero_grad()
         loss = loss_fn(param)
         loss.backward()
         optimizer.step()
 
-    # -------------------------------------------- First step of optimization
-
-    # Manual first step of optimization. The gradient is simply the current value of p.
+    # Initialization of PID hyperparameters
+    error_minus_1 = torch.zeros_like(param)
+    delta_minus_1 = torch.zeros_like(param)
     p0 = param.clone().detach()
-    p1 = p0 - lr * PID_direction(p0, 0.0, 0.0)
 
-    do_step()
+    # -------------------------------------------- First step of optimization
+    # Manual first step of optimization. The gradient is simply the current value of p.
+    error_0 = -compute_gradient()
+    error_change_0 = error_0 - error_minus_1
+    delta_0 = update_ema(delta_minus_1, error_change_0)
+    delta_change_0 = delta_0 - delta_minus_1
+    p1 = p0 + lr * recursive_PID_direction(error_0, error_change_0, delta_change_0)
 
+    do_optimizer_step()
+
+    # Check that iterates match
     assert torch.allclose(param, p1)
 
-    # Check the state of the optimizer. Should contain the first gradient in
-    # `previous_grad`, but not `delta_ema` yet.
-    assert torch.allclose(optimizer.state[param]["previous_grad"], p0)
-    assert "delta_ema" not in optimizer.state[param]
+    # When entering the next iteration, the "current" error and delta become the "previous"
+    if Kp != 0 or Ki != 0:
+        assert torch.allclose(optimizer.state[param]["previous_error"], error_0)
+    if Kd != 0:
+        assert torch.allclose(optimizer.state[param]["previous_delta"], delta_0)
 
     # -------------------------------------------- Second step of optimization
-    # The delta_ema is initialized to the difference of the first two gradients.
-    # However, it is not used for the second step of optimization.
-    delta_ema = p1 - p0
-    p2 = p1 - lr * PID_direction(p1, p1 - p0, 0.0)
+    error_1 = -compute_gradient()
+    error_change_1 = error_1 - error_0
+    delta_1 = update_ema(delta_0, error_change_1)
+    delta_change_1 = delta_1 - delta_0
+    p2 = p1 + lr * recursive_PID_direction(error_1, error_change_1, delta_change_1)
 
-    do_step()
+    do_optimizer_step()
 
+    # Check that iterates match
     assert torch.allclose(param, p2)
 
-    # The state contain p1 in `previous_grad` and p1 - p0 in `delta_ema`.
-    assert torch.allclose(optimizer.state[param]["previous_grad"], p1)
-    assert torch.allclose(optimizer.state[param]["delta_ema"], delta_ema)
+    # When entering the next iteration, the "current" error and delta become the "previous"
+    if Kp != 0 or Ki != 0:
+        assert torch.allclose(optimizer.state[param]["previous_error"], error_1)
+    if Kd != 0:
+        assert torch.allclose(optimizer.state[param]["previous_delta"], delta_1)
 
     # -------------------------------------------- Third step of optimization
-    new_delta_ema = update_ema(delta_ema, p2 - p1)
-    p3 = p2 - lr * PID_direction(p2, p2 - p1, new_delta_ema - delta_ema)
+    error_2 = -compute_gradient()
+    error_change_2 = error_2 - error_1
+    delta_2 = update_ema(delta_1, error_change_2)
+    delta_change_2 = delta_2 - delta_1
+    p3 = p2 + lr * recursive_PID_direction(error_2, error_change_2, delta_change_2)
 
-    do_step()
+    do_optimizer_step()
 
+    # Check that iterates match
     assert torch.allclose(param, p3)
 
-    # The state contain p2 in `previous_grad` and p2 - p1 in `delta_ema`.
-    assert torch.allclose(optimizer.state[param]["previous_grad"], p2)
-    assert torch.allclose(optimizer.state[param]["delta_ema"], new_delta_ema)
+    # When entering the next iteration, the "current" error and delta become the "previous"
+    if Kp != 0 or Ki != 0:
+        assert torch.allclose(optimizer.state[param]["previous_error"], error_2)
+    if Kd != 0:
+        assert torch.allclose(optimizer.state[param]["previous_delta"], delta_2)
 
 
-@pytest.mark.parametrize(["Kp", "Ki", "Kd", "ema_beta"], ALL_HYPER_PARAMS)
-def test_manual_sparse_pid(Kp, Ki, Kd, ema_beta):
+@pytest.mark.parametrize(["Kp", "Ki", "Kd", "ema_nu"], ALL_HYPER_PARAMS)
+def test_manual_sparse_pid(Kp, Ki, Kd, ema_nu):
     if not torch.cuda.is_available():
         pytest.skip("Sparse gradients are only supported on GPU.")
 
@@ -94,7 +122,7 @@ def test_manual_sparse_pid(Kp, Ki, Kd, ema_beta):
     def PID_direction(grad, change, curvature):
         return Ki * grad + Kp * change + Kd * curvature
 
-    optimizer = PID(param.parameters(), lr=lr, Kp=Kp, Ki=Ki, Kd=Kd, ema_beta=ema_beta)
+    optimizer = PID(param.parameters(), lr=lr, Kp=Kp, Ki=Ki, Kd=Kd, ema_nu=ema_nu)
 
     def do_step(indices):
         optimizer.zero_grad()
@@ -111,28 +139,28 @@ def test_manual_sparse_pid(Kp, Ki, Kd, ema_beta):
     p1[first_indices] -= lr * PID_direction(p0, 0.0, 0.0)[first_indices]  # only update the current indices
 
     # Manually calculate the state of the optimizer
-    previous_grad = torch.zeros_like(p0)
-    previous_grad[first_indices] = p0.clone()[first_indices]
-    # For the first step, the `delta_ema` is 0 for all indices
-    delta_ema = torch.zeros_like(p0)
+    previous_error = torch.zeros_like(p0)
+    previous_error[first_indices] = p0.clone()[first_indices]
+    # For the first step, the `previous_delta` is 0 for all indices
+    previous_delta = torch.zeros_like(p0)
 
     do_step(first_indices)
 
     assert torch.allclose(param(all_indices), p1)
 
     # Check the state of the optimizer. For observer indices, should contain the first
-    # gradient in `previous_grad` and also in `delta_ema`. 0 otherwise.
+    # gradient in `previous_error` and also in `previous_delta`. 0 otherwise.
     state = optimizer.state[list(param.parameters())[0]]
 
-    assert torch.allclose(state["previous_grad"].flatten(), previous_grad)
-    assert torch.allclose(state["delta_ema"].flatten(), delta_ema)
+    assert torch.allclose(state["previous_error"].flatten(), previous_error)
+    assert torch.allclose(state["previous_delta"].flatten(), previous_delta)
 
     # -------------------------------------------- Second step of optimization
     second_indices = torch.tensor([0, 3, 5, 7, 9], device=device)
 
     # Manual second step
     p2 = p1.clone()
-    change = p1 - previous_grad
+    change = p1 - previous_error
 
     unseen_indices = torch.ones_like(all_indices, dtype=torch.bool)
     unseen_indices[first_indices] = False
@@ -140,12 +168,12 @@ def test_manual_sparse_pid(Kp, Ki, Kd, ema_beta):
 
     p2[second_indices] -= lr * PID_direction(p1, change, 0.0)[second_indices]
 
-    previous_grad[second_indices] = p1.clone()[second_indices]
+    previous_error[second_indices] = p1.clone()[second_indices]
 
-    # For indices that were observed before *and* now, the delta_ema is p1 - p0.
+    # For indices that were observed before *and* now, the previous_delta is p1 - p0.
     # Note that no EMA calculation is being applied yet.
     twice_observed_indices = [idx for idx in range(10) if idx in first_indices and idx in second_indices]
-    delta_ema[twice_observed_indices] = p1[twice_observed_indices] - p0[twice_observed_indices]
+    previous_delta[twice_observed_indices] = p1[twice_observed_indices] - p0[twice_observed_indices]
 
     do_step(second_indices)
 
@@ -153,8 +181,8 @@ def test_manual_sparse_pid(Kp, Ki, Kd, ema_beta):
 
     state = optimizer.state[list(param.parameters())[0]]
 
-    assert torch.allclose(state["previous_grad"].flatten(), previous_grad)
-    assert torch.allclose(state["delta_ema"].flatten(), delta_ema)
+    assert torch.allclose(state["previous_error"].flatten(), previous_error)
+    assert torch.allclose(state["previous_delta"].flatten(), previous_delta)
 
     #  -------------------------------------------- Third step of optimization
     third_indices = torch.tensor([0, 2, 4, 7], device=device)
@@ -162,31 +190,33 @@ def test_manual_sparse_pid(Kp, Ki, Kd, ema_beta):
     # Manual third step
     p3 = p2.clone()
 
-    # Change is simply p2 - previous_grad
-    change = p2 - previous_grad
+    # Change is simply p2 - previous_error
+    change = p2 - previous_error
 
-    # Calculate the delta_ema
-    new_delta_ema = delta_ema.clone()
+    # Calculate the previous_delta
+    new_previous_delta = previous_delta.clone()
     # * For indices observed only once in the past, and also now:
-    #  * If they are part of first_indices, the delta_ema is p2 - p0
+    #  * If they are part of first_indices, the previous_delta is p2 - p0
     observed_now_and_first = [idx for idx in range(10) if idx in first_indices and idx in third_indices]
-    new_delta_ema[observed_now_and_first] = p2[observed_now_and_first] - p0[observed_now_and_first]
-    #  * If they are part of second_indices, the delta_ema is p2 - p1
+    new_previous_delta[observed_now_and_first] = p2[observed_now_and_first] - p0[observed_now_and_first]
+    #  * If they are part of second_indices, the previous_delta is p2 - p1
     observed_now_and_second = [idx for idx in range(10) if idx in second_indices and idx in third_indices]
-    new_delta_ema[observed_now_and_second] = p2[observed_now_and_second] - p1[observed_now_and_second]
-    # * For indices observed twice in the past, delta_ema is:
-    #   ema_beta * delta_ema + (1 - ema_beta) * change
+    new_previous_delta[observed_now_and_second] = p2[observed_now_and_second] - p1[observed_now_and_second]
+    # * For indices observed twice in the past, previous_delta is:
+    #   ema_nu * previous_delta + (1 - ema_nu) * change
     observed_thrice = [
         idx for idx in range(10) if idx in first_indices and idx in second_indices and idx in third_indices
     ]
 
-    new_delta_ema[observed_thrice] = ema_beta * delta_ema[observed_thrice] + (1 - ema_beta) * change[observed_thrice]
+    new_previous_delta[observed_thrice] = (
+        ema_nu * previous_delta[observed_thrice] + (1 - ema_nu) * change[observed_thrice]
+    )
 
-    # Although the new_delta_ema is calculated for indices from `observer_now_and_first`
-    # and `observer_now_and_second`, since the old delta_ema was 0 for those indices,
+    # Although the new_previous_delta is calculated for indices from `observer_now_and_first`
+    # and `observer_now_and_second`, since the old previous_delta was 0 for those indices,
     # curvature is not estimated for them.
     curvature = torch.zeros_like(p2)
-    curvature[observed_thrice] = new_delta_ema[observed_thrice] - delta_ema[observed_thrice]
+    curvature[observed_thrice] = new_previous_delta[observed_thrice] - previous_delta[observed_thrice]
 
     p3[third_indices] -= lr * PID_direction(p2, change, curvature)[third_indices]
 
@@ -195,15 +225,15 @@ def test_manual_sparse_pid(Kp, Ki, Kd, ema_beta):
 
     assert torch.allclose(param(all_indices), p3)
 
-    previous_grad[third_indices] = p2.clone()[third_indices]
+    previous_error[third_indices] = p2.clone()[third_indices]
 
-    assert torch.allclose(state["previous_grad"].flatten(), previous_grad)
-    assert torch.allclose(state["delta_ema"].flatten(), new_delta_ema)
+    assert torch.allclose(state["previous_error"].flatten(), previous_error)
+    assert torch.allclose(state["previous_delta"].flatten(), new_previous_delta)
 
 
-@pytest.mark.parametrize(["Kp", "Ki", "Kd", "ema_beta"], ALL_HYPER_PARAMS)
+@pytest.mark.parametrize(["Kp", "Ki", "Kd", "ema_nu"], ALL_HYPER_PARAMS)
 def test_pid_convergence(
-    Kp, Ki, Kd, ema_beta, Toy2dCMP_problem_properties, Toy2dCMP_params_init, use_multiple_primal_optimizers, device
+    Kp, Ki, Kd, ema_nu, Toy2dCMP_problem_properties, Toy2dCMP_params_init, use_multiple_primal_optimizers, device
 ):
     """Test convergence of PID updates on toy 2D problem. The PID updates are only
     applied to the dual variables."""
@@ -219,7 +249,7 @@ def test_pid_convergence(
     )
 
     dual_params = [{"params": constraint.multiplier.parameters()} for constraint in cmp.constraint_groups]
-    dual_optimizer = PID(dual_params, lr=0.01, Kp=Kp, Ki=Ki, Kd=Kd, ema_beta=ema_beta, maximize=True)
+    dual_optimizer = PID(dual_params, lr=0.01, Kp=Kp, Ki=Ki, Kd=Kd, ema_nu=ema_nu, maximize=True)
 
     cooper_optimizer = cooper.optim.SimultaneousOptimizer(
         primal_optimizers, dual_optimizer, constraint_groups=cmp.constraint_groups
