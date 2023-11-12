1ae67216d26285e293b79d018870d74551015b52
Jose Gallego-Posada <8620503+gallego-posada@users.noreply.github.com>
2023-11-11 20:16:35 -0500

    Update MNIST LogReg tutorial

--- tutorials/scripts/plot_mnist_logistic_regression.py
+++ tutorials/scripts/plot_mnist_logistic_regression.py
@@ -6,12 +6,11 @@ Here we consider a simple convex constrained optimization problem that involves
 training a Logistic Regression clasifier on the MNIST dataset. The model is
 constrained so that the squared L2 norm of its parameters is less than 1.
 
-This example illustrates how **Cooper** integrates with:
-    -  constructing a ``cooper.LagrangianFormulation`` and a ``cooper.SimultaneousOptimizer``
-    -  models defined using a ``torch.nn.Module``,
+This example illustrates how **Cooper** integrates with:`
+    - constructing a``cooper.optim.SimultaneousOptimizer``
+    - models defined using a ``torch.nn.Module``,
     - CUDA acceleration,
     - typical machine learning training loops,
-    - extracting the value of the Lagrange multipliers from a ``cooper.LagrangianFormulation``.
 """
 
 import matplotlib.pyplot as plt
@@ -21,8 +20,7 @@ from style_utils import *
 from torchvision import datasets, transforms
 
 import cooper
-from cooper import CMPState, ConstraintGroup, ConstraintState, ConstraintType
-from cooper.optim import SimultaneousOptimizer
+
 
 np.random.seed(0)
 torch.manual_seed(0)
@@ -46,18 +44,24 @@ model = model.to(DEVICE)
 primal_optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, amsgrad=True)
 
 # Define the constraint group for the norm constraint
-ineq_group = ConstraintGroup(
-    constraint_type=ConstraintType.INEQUALITY, multiplier_kwargs={"shape": 1, "device": DEVICE}
+multiplier = cooper.multipliers.DenseMultiplier(
+    constraint_type=cooper.ConstraintType.INEQUALITY, num_constraints=1, device=DEVICE
+)
+norm_constraint = cooper.ConstraintGroup(
+    constraint_type=cooper.ConstraintType.INEQUALITY,
+    formulation_type=cooper.FormulationType.LAGRANGIAN,
+    multiplier=multiplier,
 )
 
 # Instantiate Pytorch optimizer class for the dual variables
-dual_optimizer = torch.optim.SGD(ineq_group.multiplier.parameters(), lr=1e-3, maximize=True)
+dual_optimizer = torch.optim.SGD(multiplier.parameters(), lr=1e-3, maximize=True)
 
-cooper_optimizer = SimultaneousOptimizer(
-    primal_optimizers=primal_optimizer, dual_optimizers=dual_optimizer, constraint_groups=ineq_group
+cooper_optimizer = cooper.optim.SimultaneousOptimizer(
+    primal_optimizers=primal_optimizer, dual_optimizers=dual_optimizer, multipliers=multiplier
 )
 
-all_metrics = {"batch_ix": [], "train_loss": [], "train_acc": [], "ineq_multiplier": [], "ineq_defect": []}
+
+all_metrics = {"batch_ix": [], "train_loss": [], "train_acc": [], "multiplier_value": [], "constraint_violation": []}
 
 batch_ix = 0
 
@@ -73,27 +77,23 @@ for epoch_num in range(7):
         accuracy = (logits.argmax(dim=1) == targets).float().mean()
 
         sq_l2_norm = model.weight.pow(2).sum() + model.bias.pow(2).sum()
-        # Constraint defects use convention “g - \epsilon ≤ 0”
-        constraint_defect = sq_l2_norm - 1.0
-        constraint_state = ConstraintState(violation=constraint_defect)
+        # Constraint violations use convention “g - \epsilon ≤ 0”
+        constraint_state = cooper.ConstraintState(violation=sq_l2_norm - 1.0)
 
         # Create a CMPState object, which contains the loss and observed constraints
-        cmp_state = CMPState(loss=loss, observed_constraints=[(ineq_group, constraint_state)])
+        cmp_state = cooper.CMPState(loss=loss, observed_constraints=[(norm_constraint, constraint_state)])
 
         cooper_optimizer.zero_grad()
         lagrangian_store = cmp_state.populate_lagrangian()
         cmp_state.backward()
         cooper_optimizer.step()
 
-        # Extract the value of the Lagrange multiplier associated with the constraint
-        multiplier_value = ineq_group.multiplier()
-
         if batch_ix % 3 == 0:
             all_metrics["batch_ix"].append(batch_ix)
             all_metrics["train_loss"].append(loss.item())
             all_metrics["train_acc"].append(accuracy.item())
-            all_metrics["ineq_multiplier"].append(multiplier_value.item())
-            all_metrics["ineq_defect"].append(constraint_defect.item())
+            all_metrics["multiplier_value"].append(multiplier().item())
+            all_metrics["constraint_violation"].append(constraint_state.violation.item())
 
 fig, (ax0, ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=4, sharex=True, figsize=(18, 4))
 
@@ -105,11 +105,11 @@ ax1.plot(all_metrics["batch_ix"], all_metrics["train_acc"])
 ax1.set_xlabel("Batch")
 ax1.set_title("Training Acc")
 
-ax2.plot(all_metrics["batch_ix"], np.stack(all_metrics["ineq_multiplier"]))
+ax2.plot(all_metrics["batch_ix"], np.stack(all_metrics["multiplier_value"]))
 ax2.set_xlabel("Batch")
 ax2.set_title("Inequality Multiplier")
 
-ax3.plot(all_metrics["batch_ix"], np.stack(all_metrics["ineq_defect"]))
+ax3.plot(all_metrics["batch_ix"], np.stack(all_metrics["constraint_violation"]))
 # Show that defect converges close to zero
 ax3.axhline(0.0, c="gray", alpha=0.5)
 ax3.set_xlabel("Batch")
