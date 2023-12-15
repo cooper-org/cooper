2a2330848a42d391134b3e9b356b3e1df30f9049
Jose Gallego-Posada <8620503+gallego-posada@users.noreply.github.com>
2023-12-14 19:42:57 -0500

    Rename PID_optimizers module

--- cooper/optim/__init__.py
+++ cooper/optim/__init__.py
@@ -3,6 +3,6 @@ from .constrained_optimizers import *
 from .extra_optimizers import ExtraAdam, ExtragradientOptimizer, ExtraSGD
 from .optimizer_state import CooperOptimizerState
 from .PI_optimizer import PI
-from .PID_optimizers import PID
+from .PID_optimizer import PID
 from .types import AlternationType
 from .unconstrained_optimizer import UnconstrainedOptimizer
