b7bd21ee9355c9d3680a8db0dba6db79c0d50190
juan43ramirez <juan43.ramirez@gmail.com>
2024-03-13 14:00:14 -0400

    Add reference in `nuPI` implementation, move to `torch_optimizers`

--- cooper/optim/__init__.py
+++ cooper/optim/__init__.py
@@ -1,6 +1,5 @@
 from . import utils
 from .constrained_optimizers import *  # noqa: F403
-from .nupi_optimizer import nuPI
 from .optimizer_state import CooperOptimizerState
 from .torch_optimizers import *  # noqa: F403
 from .types import AlternationType
