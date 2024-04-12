2b7eab414a50bde02528aa93e2a34ae5452fc66e
Meraj Hashemizadeh <merajhashemi@yahoo.co.uk>
2024-04-12 17:30:49 -0400

    Use new `primal_step` method in test

--- tests/test_extrapolation.py
+++ tests/test_extrapolation.py
@@ -61,8 +61,7 @@ def test_manual_extrapolation(Toy2dCMP_problem_properties, Toy2dCMP_params_init,
     for constraint, constraint_state in cmp_state.observed_constraints.items():
         assert torch.allclose(constraint.multiplier.weight.grad, constraint_state.violation)
 
-    for primal_optimizer in cooper_optimizer.primal_optimizers:
-        primal_optimizer.extrapolation()
+    cooper_optimizer.primal_step()
     cooper_optimizer.dual_step(call_extrapolation=True)
 
     # Perform the actual update step
