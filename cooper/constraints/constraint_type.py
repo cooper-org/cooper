e492569e04bcb01d7a2c8d67f03ca52ff2223752
Jose Gallego-Posada <8620503+gallego-posada@users.noreply.github.com>
2024-05-14 10:43:57 -0400

    Add TODO

--- cooper/constraints/constraint_type.py
+++ cooper/constraints/constraint_type.py
@@ -1,6 +1,8 @@
 from enum import Enum, auto
 
 
+# TODO(gallego-posada): Maybe we can move this to the `constraint` module. Need to
+#  ensure no circular imports are created.
 class ConstraintType(Enum):
     """Constraint type enumeration."""
 
