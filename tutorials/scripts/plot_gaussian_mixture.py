51213788c210359678ff61bbb34065ce5031a62e
juan43ramirez <juan43.ramirez@gmail.com>
2024-05-15 15:30:24 -0400

    Updated plot_gaussian_mixture

--- tutorials/scripts/plot_gaussian_mixture.py
+++ tutorials/scripts/plot_gaussian_mixture.py
@@ -2,31 +2,62 @@ r"""
 Linear classification with rate constraints
 ===============================================
 
-In this example we consider a linear classification problem on synthetically
-mixture of Gaussians data. We constraint the model to predict at least 70% of
-the training points as blue.
+In this example we consider a linear classification problem on a synthetically generated
+mixture of Gaussians. We constrain the model to predict at least 70% of the training
+points as class blue (class 0). The optimization problem is defined as follows:
 
-Note that this is a non-differentiable constraint, and thus the typical
-Lagrangian approach is not applicable as it requires to compute the derivatives
-of the constraints :math:`g` and :math:`h`.
+.. math::
+    min_{w, b} \mathbb{E}_{(x, y) \sim \mathcal{D}} \left[ \ell(w^T x + b, y) \right]
+    s.t. \mathbb{E}_{(x, y) \sim \mathcal{D}} \mathbb{1}_{\sigma(w^T x + b) \leq 0} \geq 0.7
+where :math:`\ell` is the binary cross-entropy loss, :math:`w` and :math:`b` are the
+weights and bias of the linear model, and :math:`x` and :math:`y` are the input and
+target labels. The expectation is computed over the data distribution
+:math:`\mathcal{D}`.
 
-A commonly used approach to deal with this difficulty is to retain the
-Lagrangian formulation, but replace the constraints with differentiable
-approximations or surrogates. However, changing the constraint functions can
-result in an over- or under-constrained version of the problem.
+Note that this constraint is not continuous on the model parameters, and so it is not
+differentiable. The typical Lagrangian approach is not applicable in this setting as
+we can not compute the derivatives of the constraint.
+
+A possible approach to deal with this difficulty is to retain the Lagrangian
+formulation, but replace the constraints with differentiable approximations or
+surrogates. However, changing the constraint functions can result in an over- or
+under-constrained version of the problem (as illustrated in this tutorial).
 
 :cite:t:`cotter2019JMLR` propose a *proxy-Lagrangian formulation*, in which the
 non-differentiable constraints are relaxed *only when necessary*. In other
 words, the non differentiable constraint functions are used to compute the
-Lagrangian and constraint violations (and thus the multiplier updates), while
-the surrogates are used to compute the primal gradients.
+Lagrangian and constraint violations (and thus the to update the Lagrange multipliers),
+while the surrogates are used to compute gradients of the Lagrangian with respect to the
+parameters of the model.
 
-This example is based on Fig. 2 of :cite:t:`cotter2019JMLR`. Here we present
-the naive setting where proxy-constraints are used on a Lagrangian formulation,
-rather than a "proper" proxy-Lagrangian formulation. For details on the
-notion of a proxy-Lagrangian formulation, see :math:`\S` 4.2 in
-:cite:p:`cotter2019JMLR`.
+**Surrogate.** The surrogate considered in this tutorial is the following:
+.. math::
+    P(\hat{y}=0) \geq 0.7
+
+where :math:`P(\hat{y}=0) = \mathbb{E}_{(x, y) \sim \mathcal{D}} P(\hat{y}=0 | x) = \mathbb{E}_{(x, y) \sim \mathcal{D}} 1 - \sigma(w^T x + b)`
+is the proportion of points predicted as class 0 by the model.
+
+Note that the surrogate is differentiable with respect to the model parameters, and it
+is aligned with the original constraint: an increase in the probability of predicting
+class 0 leads to an increase in the proportion of points predicted as class 0.
+
+**Results.** The plot shows the decision boundary of the linear model trained with
+three different formulations: unconstrained, constrained with the surrogate constraint,
+and constrained with proxy constraints.
+* As evidenced in the leftmost plot, the proportion of points predicted as class 0 when
+    training without constraints is 50%. This is to be expected, as the model is
+    optimizing for binary cross-entropy over a balanced and (almost) separable dataset.
+* The middle plot shows the decision boundary of the model trained with the surrogate
+    constraint. The proportion of points predicted as class 0 is 66%, which is below the
+    desired 70%. This is due to the relaxation of the constraint, which is different
+    from the original constraint.
+* The rightmost plot shows the decision boundary of the model trained with the proxy
+    constraint. The proportion of points predicted as class 0 is 70%, thus yielding a
+    feasible solution.
+
+This example is based on Fig. 2 of :cite:t:`cotter2019JMLR`.
 """
+
 import itertools
 import random
 
@@ -49,23 +80,23 @@ def generate_mog_dataset():
     Generate a MoG dataset on 2D, with two classes.
     """
 
-    n_per_class = 100
+    n_samples_per_class = 100
     dim = 2
     n_gaussians = 4
-    mus = [(0, 1), (-1, 0), (0, -1), (1, 0)]
-    mus = [torch.tensor(m) for m in mus]
+    means = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+    means = [torch.tensor(m) for m in means]
     var = 0.05
 
     inputs, labels = [], []
 
     for id in range(n_gaussians):
         # Generate input data by mu + x @ sqrt(cov)
-        cov = np.sqrt(var) * torch.eye(dim)
-        mu = mus[id]
-        inputs.append(mu + torch.randn(n_per_class, dim) @ cov)
+        cov = np.sqrt(var) * torch.eye(dim)  # Diagonal covariance matrix
+        mu = means[id]
+        inputs.append(mu + torch.randn(n_samples_per_class, dim) @ cov)
 
         # Labels
-        labels.append(torch.tensor(n_per_class * [1.0 if id < 2 else 0.0]))
+        labels.append(torch.tensor(n_samples_per_class * [1.0 if id < 2 else 0.0]))
 
     return torch.cat(inputs, dim=0), torch.cat(labels, dim=0)
 
@@ -98,8 +129,11 @@ class MixtureSeparation(cooper.ConstrainedMinimizationProblem):
     Implements CMP for separating the MoG dataset with a linear predictor.
 
     Args:
-        use_proxy: Flag to use proxy-constraints. If ``True``, we use a hinge
-            relaxation. Defaults to ``False``.
+        use_strict_constraints: Flag to use proxy-constraints. If ``False``, we use a
+            hinge relaxation both for updating the Lagrange multipliers and for updating
+            the model parameters. If ``True``, we use the hinge relaxation only for the
+            model parameters, and the true rate of blue predictions for updating the
+            Lagrange multipliers. Defaults to ``False``.
         constraint_level: Minimum proportion of points to be predicted as belonging to
             the blue class. Ignored when ``is_constrained==False``. Defaults to ``0.7``.
     """
@@ -108,10 +142,9 @@ class MixtureSeparation(cooper.ConstrainedMinimizationProblem):
         super().__init__()
 
         constraint_type = cooper.ConstraintType.INEQUALITY
+        multiplier = cooper.multipliers.DenseMultiplier(constraint_type=constraint_type, num_constraints=1)
         self.rate_constraint = cooper.Constraint(
-            constraint_type=constraint_type,
-            formulation_type=cooper.LagrangianFormulation,
-            multiplier=cooper.multipliers.DenseMultiplier(constraint_type=constraint_type, num_constraints=1),
+            constraint_type=constraint_type, formulation_type=cooper.LagrangianFormulation, multiplier=multiplier
         )
 
         self.constraint_level = constraint_level
@@ -121,30 +154,30 @@ class MixtureSeparation(cooper.ConstrainedMinimizationProblem):
         logits = model(inputs)
         loss = torch.nn.functional.binary_cross_entropy_with_logits(logits.flatten(), targets)
 
-        # Separating classes s.t. predicting at least constraint_level as class 0
-
         # Hinge approximation of the rate
         probs = torch.sigmoid(logits)
 
-        # The differentiable violation uses the hinge loss as a surrogate
-        hinge = torch.mean(torch.max(torch.zeros_like(probs), 1 - probs))
-        differentiable_violation = -hinge
+        # Surrogate "constraint": prob_0 >= constraint_level -> constraint_level - prob_0 <= 0
+        differentiable_violation = self.constraint_level - torch.mean(1 - probs)
 
-        strict_violation = None
         if self.use_strict_constraints:
-            # Use strict violations to update the Lagrange multipliers
+            # Use the true rate of blue predictions as the constraint
             classes = logits >= 0.0
             prop_0 = torch.sum(classes == 0) / targets.numel()
+
+            # Constraint: prop_0 >= constraint_level -> constraint_level - prop_0 <= 0
             strict_violation = self.constraint_level - prop_0
 
-        constraint_state = cooper.ConstraintState(violation=differentiable_violation, strict_violation=strict_violation)
+            constraint_state = cooper.ConstraintState(
+                violation=differentiable_violation, strict_violation=strict_violation
+            )
+        else:
+            constraint_state = cooper.ConstraintState(violation=differentiable_violation)
+
         return cooper.CMPState(loss=loss, observed_constraints={self.rate_constraint: constraint_state})
 
 
 def train(problem_name, inputs, targets, num_iters=5000, lr=1e-2, constraint_level=0.7):
-    """
-    Train via SGD
-    """
 
     is_constrained = "const" in problem_name.lower()
     use_strict_constraints = "proxy" in problem_name.lower()
@@ -175,7 +208,7 @@ def train(problem_name, inputs, targets, num_iters=5000, lr=1e-2, constraint_lev
 
 
 # Plot configs
-titles = ["Unconstrained", "Const+Surrogate", "Const+Proxy"]
+titles = ["Unconstrained", "Const w/ Surrogate", "Proxy Const"]
 fig, axs = plt.subplots(1, 3, figsize=(18, 6))
 
 # Data and training configs
