6ecc2a26b414f855e1709e928d6fa92dabd4cc69
Meraj Hashemizadeh <merajhashemi@yahoo.co.uk>
2024-06-03 17:24:50 -0400

    Remove unused test util functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 import pytest
 import torch
 
-from tests.helpers import testing_utils
 from tests.helpers.cooper_test_utils import (  # noqa:  F401
     Toy2dCMP_params_init,
     Toy2dCMP_problem_properties,
@@ -11,8 +10,7 @@ from tests.helpers.cooper_test_utils import (  # noqa:  F401
 
 @pytest.fixture(scope="session", params=["cpu", "cuda"])
 def device(request):
-    device, skip, skip_reason = testing_utils.get_device_or_skip(request.param, torch.cuda.is_available())
-    if skip:
-        pytest.skip(skip_reason)
+    if request.param == "cuda" and not torch.cuda.is_available():
+        pytest.skip("Aim device 'cuda' is not available.")
 
-    return device
+    return torch.device(device)
