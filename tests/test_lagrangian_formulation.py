c7b5a61ecb806d0ca5f2b6442b6d1fd3ed4d5472
lucas-maes <lucas.maes@umontreal.ca>
2024-06-03 19:59:20 -0400

    add damping violation test

--- tests/test_lagrangian_formulation.py
+++ tests/test_lagrangian_formulation.py
@@ -55,3 +55,23 @@ def test_damped_lagrangian_formulation():
 
     lf.create_state(cmp.state)
     assert (lf.ineq_multipliers is not None) and (lf.eq_multipliers is not None)
+
+    # Check correct violation of constraints
+    lf = cooper.DampedLagrangianFormulation(cmp, damping_coefficient)
+    cmp.state = cooper.CMPState(
+        eq_defect=torch.tensor([1.0]), ineq_defect=torch.tensor([1.0, 1.2])
+    )
+
+    lf.create_state(cmp.state)
+
+    # Check that the weighted violation is correct for equality constraints
+    damped_eq_mult = (lf.eq_multipliers() - damping_coefficient) * cmp.state.eq_defect
+    eq_violation = torch.sum(damped_eq_mult * cmp.state.eq_defect)
+
+    assert torch.allclose(lf.weighted_violation(cmp.state, constraint_type="eq"), eq_violation)
+
+    # Check that the weighted violation is correct for inequality constraints
+    damped_ineq_mult = (lf.ineq_multipliers() - damping_coefficient) * cmp.state.ineq_defect
+    ineq_violation = torch.sum(damped_ineq_mult * cmp.state.ineq_defect)
+
+    assert torch.allclose(lf.weighted_violation(cmp.state, constraint_type="ineq"), ineq_violation)
