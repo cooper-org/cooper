670747ba92f50acfcd026a05d1ff865182be1cc9
Meraj Hashemizadeh <merajhashemi@yahoo.co.uk>
2024-06-27 21:41:08 -0400

    Remove importlib for python 3.7 and older

--- cooper/__init__.py
+++ cooper/__init__.py
@@ -1,11 +1,6 @@
 """Top-level package for Cooper."""
 
-import sys
-
-if sys.version_info >= (3, 8):
-    from importlib.metadata import PackageNotFoundError, version
-else:
-    from importlib_metadata import PackageNotFoundError, version
+from importlib.metadata import PackageNotFoundError, version
 
 try:
     __version__ = version("cooper")
